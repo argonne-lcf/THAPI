0e2aeffde90fb9bbe829ec3bef6e364bbc616f3f
Brice Videau <bvideau@anl.gov>
2019-12-16 15:08:02 -0600

    Parse ddi headers. Might be unnecessary...

--- ze/extract_ze.rb
+++ ze/extract_ze.rb
@@ -2,6 +2,7 @@ require_relative 'extract_base.rb'
 
 preprocessed_sources_ze_api = $cpp.preprocess(<<EOF).gsub(/^#.*?$/, '')
 #include <ze_api.h>
+#include <ze_ddi.h>
 EOF
 
 ast = $parser.parse(preprocessed_sources_ze_api)
