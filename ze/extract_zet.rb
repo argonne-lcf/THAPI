7fc91f4766dda3a0ca27ea5dc1cf494e1cb5e356
Brice Videau <bvideau@anl.gov>
2019-12-20 09:48:54 -0600

    Commented ddi interface for now.

--- ze/extract_zet.rb
+++ ze/extract_zet.rb
@@ -2,20 +2,20 @@ require_relative 'extract_base.rb'
 
 preprocessed_sources_ze_api = $cpp.preprocess(<<EOF).gsub(/^#.*?$/, '')
 #include <ze_api.h>
-#include <ze_ddi.h>
+//#include <ze_ddi.h>
 EOF
 
 preprocessed_sources_zex_api = $cpp.preprocess(<<EOF).gsub(/^#.*?$/, '')
 #define _ZE_API_H
 #include <zex_api.h>
-#include <zex_ddi.h>
+//#include <zex_ddi.h>
 EOF
 
 preprocessed_sources_zet_api = $cpp.preprocess(<<EOF).gsub(/^#.*?$/, '')
 #define _ZE_API_H
 #define _ZEX_API_H
 #include <zet_api.h>
-#include <zet_ddi.h>
+//#include <zet_ddi.h>
 EOF
 
 $parser.parse(preprocessed_sources_ze_api)
