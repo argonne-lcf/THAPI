8a4757833fe7485b0a6c164d99164649f5a3fb1d
Brice Videau <bvideau@anl.gov>
2020-01-06 16:33:04 -0600

    Added preliminary support for INTEL extension functions.

--- opencl/gen_opencl_header.rb
+++ opencl/gen_opencl_header.rb
@@ -7,5 +7,13 @@ typedef enum {
     CL_ICDL_NAME=3,
     CL_ICDL_VENDOR=4,
   } cl_icdl_info;
+typedef cl_bitfield  cl_mem_alloc_flags_intel;
+typedef cl_bitfield  cl_mem_properties_intel;
+typedef cl_mem_flags cl_mem_flags_intel;
+typedef cl_uint      cl_mem_info_intel;
+typedef cl_uint      cl_mem_advice_intel;
+typedef cl_uint      cl_unified_shared_memory_type_intel;
+typedef cl_bitfield  cl_unified_shared_memory_capabilities_intel;
+typedef cl_uint      cl_execution_info_intel;
 #endif
 EOF
