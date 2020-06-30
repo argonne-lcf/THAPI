335aadef94724f9db56638a30d7203f76a1c06d6
Brice Videau <bvideau@anl.gov>
2020-06-30 14:20:30 -0500

    Split callback declarations.

---
+++ opencl/gen_babeltrace_cl_callbacks.rb
@@ -0,0 +1,45 @@
+require 'yaml'
+opencl_model = YAML::load_file("opencl_model.yaml")
+
+puts <<EOF
+#ifndef _HEADER_CALLBACKS_H
+#define _HEADER_CALLBACKS_H
+#define CL_TARGET_OPENCL_VERSION 300
+#define CL_USE_DEPRECATED_OPENCL_1_0_APIS
+#define CL_USE_DEPRECATED_OPENCL_1_1_APIS
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
+#define CL_USE_DEPRECATED_OPENCL_2_1_APIS
+#define CL_USE_DEPRECATED_OPENCL_2_2_APIS
+#include <CL/opencl.h>
+#include <CL/cl_gl_ext.h>
+#include <CL/cl_egl.h>
+#include <CL/cl_ext_intel.h>
+#include "tracer_opencl.h"
+
+#include "babeltrace_cl.h"
+
+EOF
+
+opencl_model["events"].each { |name, fields|
+  puts <<EOF
+typedef void (#{name.gsub(":","_")}_cb)(
+    #{(["const bt_event *bt_event"]+fields.each.collect { |n, f|
+  s =  "#{f["type"].gsub("cl_errcode", "cl_int")}"
+  s << " *" if f["pointer"]
+  s << " *" if f["array"]
+  s << " *" if f["string"]
+  s << " #{n}"
+  if f["array"]
+    ["size_t _#{n}_length", s]
+  else
+    s
+  end
+}).flatten.join(",\n    ")});
+
+EOF
+}
+
+puts <<EOF
+#endif
+EOF
--- opencl/gen_babeltrace_cl_dispatchers.rb
+++ opencl/gen_babeltrace_cl_dispatchers.rb
@@ -1,22 +1,9 @@
 require 'yaml'
 opencl_model = YAML::load_file("opencl_model.yaml")
-infos = YAML::load_file("opencl_infos.yaml")
 
 puts <<EOF
-#define CL_TARGET_OPENCL_VERSION 300
-#define CL_USE_DEPRECATED_OPENCL_1_0_APIS
-#define CL_USE_DEPRECATED_OPENCL_1_1_APIS
-#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
-#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
-#define CL_USE_DEPRECATED_OPENCL_2_1_APIS
-#define CL_USE_DEPRECATED_OPENCL_2_2_APIS
-#include <CL/opencl.h>
-#include <CL/cl_gl_ext.h>
-#include <CL/cl_egl.h>
-#include <CL/cl_ext_intel.h>
-#include "tracer_opencl.h"
-
 #include "babeltrace_cl.h"
+#include "babeltrace_cl_callbacks.h"
 
 EOF
 
@@ -85,20 +72,6 @@ end
 
 opencl_model["events"].each { |name, fields|
   puts <<EOF
-typedef void (#{name.gsub(":","_")}_cb)(
-    #{(["const bt_event *bt_event"]+fields.each.collect { |n, f|
-  s =  "#{f["type"].gsub("cl_errcode", "cl_int")}"
-  s << " *" if f["pointer"]
-  s << " *" if f["array"]
-  s << " *" if f["string"]
-  s << " #{n}"
-  if f["array"]
-    ["size_t _#{n}_length", s]
-  else
-    s
-  end
-}).flatten.join(",\n    ")});
-
 void #{name.gsub(":","_")}_dispatcher(
     struct opencl_dispatch  *opencl_dispatch,
     struct opencl_callbacks *callbacks,
