49e68133f4e2a12f4353cb28f2e92e58a8fbedfe
Brice Videau <bvideau@anl.gov>
2020-08-17 14:56:01 +0000

    Extract headers for zes.

---
+++ ze/extract_zes.rb
@@ -0,0 +1,18 @@
+require_relative 'extract_base.rb'
+
+preprocessed_sources_ze_api = $cpp.preprocess(<<EOF).gsub(/^#.*?$/, '')
+#include <ze_api.h>
+#include <ze_ddi.h>
+EOF
+
+preprocessed_sources_zes_api = $cpp.preprocess(<<EOF).gsub(/^#.*?$/, '')
+#define _ZE_API_H
+#include <zes_api.h>
+#include <zes_ddi.h>
+EOF
+
+$parser.parse(preprocessed_sources_ze_api)
+ast = $parser.parse(preprocessed_sources_zes_api)
+File::open("zes_api.yaml", "w") { |f|
+  f.puts ast.extract_declarations.to_yaml
+}
