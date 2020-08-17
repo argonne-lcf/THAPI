0a275a43619a3762c3843547871e60cb135f202c
Brice Videau <bvideau@anl.gov>
2020-08-17 17:03:05 +0000

    Updated ze_meta_parameters.yaml.

--- ze/ze_model.rb
+++ ze/ze_model.rb
@@ -450,6 +450,40 @@ class InString < MetaParameter
   end
 end
 
+class OutString < MetaParameter
+  prepend Out
+  def initialize(command, name, size = nil)
+    super(command, name)
+    a = command[name]
+    s = nil
+    s = command[size] if size
+    raise "Invalid parameter: #{name} for #{command.name}!" unless a
+    t = a.type
+    raise "Type is not a pointer: #{t}!" if !t.kind_of?(YAMLCAst::Pointer)
+    ev = LTTng::TracepointField::new
+    if s
+      ev.macro = :ctf_sequence_text
+      if s.type.kind_of?(YAMLCAst::Pointer)
+        checks = check_for_null("#{size}") + check_for_null("#{name}")
+        sz = sanitize_expression("*#{size}", checks)
+        st = "#{s.type.type}"
+      else
+        checks = check_for_null("#{name}")
+        sz = sanitize_expression("#{size}", checks)
+        st = "#{s.type}"
+      end
+      ev.type = "char"
+      ev.length = sz
+      ev.length_type = st
+    else
+      ev.macro = :ctf_string
+    end
+    ev.name = "#{name}_val"
+    ev.expression = sanitize_expression("#{name}")
+    @lttng_out_type = ev
+  end
+end
+
 class ScalarMetaParameter < MetaParameter
   attr_reader :type
 
