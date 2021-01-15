a9f6950304900635d8df0b05d4e08439df0ab44d
Thomas Applencourt <tapplencourt@anl.gov>
2021-01-15 16:55:39 +0000

    Merge clprof{,testing} in the same pluging

--- opencl/test_wrapper.sh
+++ opencl/test_wrapper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 name="$(basename $1 .dust)"
-echo babeltrace2 --plugin-path=./.libs --component=source.dust.input --params="path=\"$1\"" --component=sink.testing_clprof.testing_dispatch --params="test=\"${name}\""
-babeltrace2 --plugin-path=./ --component=source.dust.input --params="path=\"$1\"" --component=sink.testing_clprof.dispatch_testing --params="test=\"${name}\""
+echo babeltrace2 --plugin-path=./.libs --component=source.dust.input --params="path=\"$1\"" --component=sink.clprof.dispatch_testing --params="test=\"${name}\""
+babeltrace2 --plugin-path=./ --component=source.dust.input --params="path=\"$1\"" --component=sink.clprof.dispatch_testing --params="test=\"${name}\""
