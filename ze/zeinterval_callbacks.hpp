3c194653737360305b17efd802f2a321ba176853
Thomas Applencourt <tapplencourt@anl.gov>
2021-02-12 17:36:37 +0000

    Add message to ze

--- ze/zeinterval_callbacks.hpp
+++ ze/zeinterval_callbacks.hpp
@@ -17,6 +17,27 @@ typedef std::tuple<uint64_t, uint64_t> timerResolution_kernelTimestampValidBits_
 
 
 struct zeinterval_callbacks_state {
-	std::queue<const bt_message*> downstream_message_queue;
+    std::unordered_map<hp_event_t,tfn_ts_t> event_to_function_name_and_ts;
+
+     std::unordered_map<hpt_function_name_t, ze_device_handle_t> function_name_to_device;
+     std::unordered_map<hp_command_list_t, dsd_t> command_list_to_device;
+     std::unordered_map<hp_device_t, timerResolution_kernelTimestampValidBits_t> device_to_timerResolution_kernelTimestampValidBits;
+
+     std::unordered_map<hpt_t, thapi_function_name> last_kernel;
+
+    std::unordered_map<hp_event_t, sd_t> event_result_to_start_and_delta;
+    std::unordered_map<hp_device_t, int64_t> device_ts_to_llng_ts;
+    std::unordered_map<hp_kernel_t, thapi_function_name> kernel_to_name;
+    std::unordered_map<hpt_function_name_t, uint64_t> host_start;
+
+    std::unordered_map<hp_device_t, std::string> device_to_name;
+    std::unordered_map<hp_device_t, thapi_device_id> device_to_rootdevice;
+    std::unordered_map<hpt_t, thapi_device_id> start_device;
+    std::unordered_map<hpt_t, thapi_function_name> profiled_function_name;
+    std::unordered_map<hpt_t, fn_ts_t> profiled_function_name_and_ts;
+    std::unordered_map<hpt_function_name_t, dsd_t> function_name_to_dsd;
+
+     std::queue<const bt_message*> downstream_message_queue;
+
 };
 
