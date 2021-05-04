0b9156b37612f3517b0518bfc14ba11de63d2070
Brice Videau <bvideau@anl.gov>
2021-05-04 15:45:21 -0500

    Search for demangle.h in libiberty folder also.

--- xprof/my_demangle.c
+++ xprof/my_demangle.c
@@ -1,5 +1,10 @@
 #include "my_demangle.h"
+#include "config.h"
+#if defined(HAVE_DEMANGLE_H)
 #include <demangle.h>
+#elif defined(HAVE_LIBIBERTY_DEMANGLE_H)
+#include <libiberty/demangle.h>
+#endif
 
 char * my_demangle(const char * name) {
     return cplus_demangle(name, DMGL_AUTO);
