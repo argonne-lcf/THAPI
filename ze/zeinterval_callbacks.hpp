626bc9836973581465736187a92c48295fc0634f
Thomas Applencourt <tapplencourt@anl.gov>
2021-11-16 23:48:20 +0000

    Update zeinterval to handle paranoid mode

--- ze/zeinterval_callbacks.hpp
+++ ze/zeinterval_callbacks.hpp
@@ -23,32 +23,20 @@ typedef std::tuple<thread_id_t, thapi_function_name, std::string, thapi_device_i
 
 struct zeinterval_callbacks_state {
     std::unordered_map<hp_event_t,tfnm_dsd_ts_ld_t> event_to_function_name_dsd_and_ts;
-    std::unordered_map<hpt_t, fnm_dsd_ts_ld_t> profiled_function_name_dsd_and_ts;
-
+    std::unordered_map<hpt_t, fnm_dsd_ts_t> profiled_function_name_dsd_and_ts;
     // https://spec.oneapi.io/level-zero/latest/core/api.html#_CPPv4N16ze_device_uuid_t2idE
     std::unordered_map<h_device_t, ze_device_uuid_t> device_to_uuid;
-
     std::unordered_map<hp_command_list_t, dsd_t> command_list_to_device;
     std::unordered_map<hp_device_t, timerResolution_kernelTimestampValidBits_t> device_to_timerResolution_kernelTimestampValidBits;
-
     std::unordered_map<hp_device_t, clock_lttng_device_t> sync_clock_lttng_device;
-
     std::unordered_map<hpt_t, thapi_function_name> last_kernel;
-
-    std::unordered_map<hp_event_t, sd_t> event_result_to_start_and_delta;
-    std::unordered_map<hp_device_t, int64_t> device_ts_to_llng_ts;
     std::unordered_map<hp_kernel_t, thapi_function_name> kernel_to_name;
     std::unordered_map<hp_kernel_t, thapi_function_name> kernel_to_groupsize;
-
     std::unordered_map<hpt_function_name_t, uint64_t> host_start;
-
-    std::unordered_map<hp_device_t, std::string> device_to_name;
     std::unordered_map<hp_device_t, thapi_device_id> device_to_rootdevice;
     std::unordered_map<hpt_t, thapi_device_id> start_device;
     std::unordered_map<hpt_t, thapi_function_name> profiled_function_name;
-
     std::queue<const bt_message*> downstream_message_queue;
-
     std::unordered_map<hpt_t, size_t> last_alloc;
     std::unordered_map<hpt_t, size_t> last_free;
     std::unordered_map<hp_t, MemoryInterval> rangeset_memory_device;
