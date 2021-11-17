97e1f2057d58d32b4dfba24c2cf0a623535ee7b0
Thomas Applencourt <tapplencourt@anl.gov>
2021-11-17 21:40:58 +0000

    Fix interval

--- ze/zeinterval_callbacks.hpp
+++ ze/zeinterval_callbacks.hpp
@@ -5,6 +5,7 @@
 #include <babeltrace2/babeltrace.h>
 #include <unordered_map>
 #include <ze_api.h>
+#include <unordered_set>
 
 typedef std::map<uintptr_t, uintptr_t> MemoryInterval;
 
@@ -13,10 +14,10 @@ typedef std::tuple<hostname_t, process_id_t, ze_kernel_handle_t> hp_kernel_t;
 
 typedef std::tuple<hostname_t, process_id_t, ze_command_list_handle_t> hp_command_list_t;
 typedef std::tuple<hostname_t, process_id_t, ze_command_queue_handle_t> hp_command_queue_t;
+typedef std::tuple<hostname_t, process_id_t ,ze_module_handle_t> hp_module_t;
 
 typedef std::tuple<uint64_t, uint64_t> timerResolution_kernelTimestampValidBits_t;
 typedef std::tuple<uint64_t, uint64_t> clock_lttng_device_t;
-
 
 typedef std::tuple<thapi_function_name, std::string, thapi_device_id, thapi_device_id, long, clock_lttng_device_t> fnm_dsd_ts_ld_t;
 typedef std::tuple<thread_id_t, thapi_function_name, std::string, thapi_device_id, thapi_device_id, long, clock_lttng_device_t> tfnm_dsd_ts_ld_t;
@@ -37,18 +38,15 @@ struct zeinterval_callbacks_state {
     std::unordered_map<hpt_t, thapi_device_id> start_device;
     std::unordered_map<hpt_t, thapi_function_name> profiled_function_name;
     std::queue<const bt_message*> downstream_message_queue;
+
+    /* State variable */
     std::unordered_map<hpt_t, size_t> last_alloc;
     std::unordered_map<hpt_t, size_t> last_free;
+    std::unordered_map<hpt_t, ze_module_handle_t> last_module;
+
+    /* Handle memory copy */
+    std::unordered_map<hp_module_t, std::unordered_set<uint64_t> > module_to_module_globals;
     std::unordered_map<hp_t, MemoryInterval> rangeset_memory_device;
     std::unordered_map<hp_t, MemoryInterval> rangeset_memory_host;
     std::unordered_map<hp_t, MemoryInterval> rangeset_memory_shared;
 };
-
-//Memory interval
-template<class T>
-bool contains(const MemoryInterval& m, const T val) {
-    const auto it = m.lower_bound(val);
-    if (it == m.cend())
-        return false;
-    return (val < it->second);
-}
