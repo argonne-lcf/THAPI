dbcbf1ab8a6146bb62462c2287dab6039950588d
Brice Videau <bvideau@anl.gov>
2021-11-19 19:08:51 -0600

    Builds.

--- ze/zeinterval_callbacks.hpp
+++ ze/zeinterval_callbacks.hpp
@@ -14,30 +14,29 @@ typedef std::tuple<hostname_t, process_id_t, ze_kernel_handle_t> hp_kernel_t;
 
 typedef std::tuple<hostname_t, process_id_t, ze_command_list_handle_t> hp_command_list_t;
 typedef std::tuple<hostname_t, process_id_t, ze_command_queue_handle_t> hp_command_queue_t;
-typedef std::tuple<hostname_t, process_id_t ,ze_module_handle_t> hp_module_t;
-
-typedef std::tuple<uint64_t, uint64_t> timerResolution_kernelTimestampValidBits_t;
+typedef std::tuple<hostname_t, process_id_t, ze_module_handle_t> hp_module_t;
+typedef hp_device_t hpd_t;
+typedef hp_event_t hpe_t;
+typedef hp_kernel_t hpk_t;
 typedef std::tuple<uint64_t, uint64_t> clock_lttng_device_t;
 
-typedef std::tuple<thapi_function_name, std::string, thapi_device_id, thapi_device_id, long, clock_lttng_device_t> fnm_dsd_ts_ld_t;
-typedef std::tuple<thread_id_t, thapi_function_name, std::string, thapi_device_id, thapi_device_id, long, clock_lttng_device_t> tfnm_dsd_ts_ld_t;
+typedef std::tuple<thread_id_t, thapi_function_name, std::string, thapi_device_id, uint64_t, clock_lttng_device_t> t_tfnm_m_d_ts_cld_t;
+typedef std::tuple<thapi_function_name, std::string, thapi_device_id, uint64_t> tfnm_m_d_ts_t;
+
+typedef std::tuple<bool, uint64_t, uint64_t> event_profiling_result_t;
 
 struct zeinterval_callbacks_state {
-    std::unordered_map<hp_event_t,tfnm_dsd_ts_ld_t> event_to_function_name_dsd_and_ts;
-    std::unordered_map<hpt_t, fnm_dsd_ts_t> profiled_function_name_dsd_and_ts;
     // https://spec.oneapi.io/level-zero/latest/core/api.html#_CPPv4N16ze_device_uuid_t2idE
-    std::unordered_map<h_device_t, ze_device_uuid_t> device_to_uuid;
-    std::unordered_map<hp_command_list_t, dsd_t> command_list_to_device;
-    std::unordered_map<hp_device_t, timerResolution_kernelTimestampValidBits_t> device_to_timerResolution_kernelTimestampValidBits;
-    std::unordered_map<hp_device_t, clock_lttng_device_t> sync_clock_lttng_device;
-    std::unordered_map<hpt_t, thapi_function_name> last_kernel;
-    std::unordered_map<hp_kernel_t, thapi_function_name> kernel_to_name;
-    std::unordered_map<hp_kernel_t, thapi_function_name> kernel_to_groupsize;
-    std::unordered_map<hpt_function_name_t, uint64_t> host_start;
-    std::unordered_map<hp_device_t, thapi_device_id> device_to_rootdevice;
-    std::unordered_map<hpt_t, thapi_device_id> start_device;
-    std::unordered_map<hpt_t, thapi_function_name> profiled_function_name;
-    std::queue<const bt_message*> downstream_message_queue;
+    std::unordered_map<hp_command_list_t, thapi_device_id>  command_list_to_device;
+    std::unordered_map<hp_device_t, ze_device_properties_t> device_to_properties;
+    std::unordered_map<hpt_t, thapi_function_name>          last_kernel;
+    std::unordered_map<hp_kernel_t, thapi_function_name>    kernel_to_name;
+    std::unordered_map<hp_kernel_t, thapi_function_name>    kernel_to_groupsize_str;
+    std::unordered_map<hpt_function_name_t, uint64_t>       host_start;
+    std::unordered_map<hp_device_t, thapi_device_id>        device_to_rootdevice;
+    std::unordered_map<hpt_t, thapi_device_id>              start_device;
+    std::unordered_map<hpt_t, thapi_function_name>          profiled_function_name;
+    std::queue<const bt_message*>                           downstream_message_queue;
 
     /* State variable */
     std::unordered_map<hpt_t, size_t> last_alloc;
@@ -49,4 +48,10 @@ struct zeinterval_callbacks_state {
     std::unordered_map<hp_t, MemoryInterval> rangeset_memory_device;
     std::unordered_map<hp_t, MemoryInterval> rangeset_memory_host;
     std::unordered_map<hp_t, MemoryInterval> rangeset_memory_shared;
+
+    std::unordered_map<hpe_t, event_profiling_result_t> event_to_profiling_result;
+    std::unordered_map<hpd_t, clock_lttng_device_t>     device_timestamps_pair_ref;
+    std::unordered_map<hpt_t, tfnm_m_d_ts_t>            command_partial_payload;
+    std::unordered_map<hpe_t, t_tfnm_m_d_ts_cld_t>      event_payload;
+    std::unordered_map<hpd_t, thapi_device_id>          subdevice_parent;
 };
