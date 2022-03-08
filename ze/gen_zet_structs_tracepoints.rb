babc30455ecbf139969c5fba82257fbaeb3fbd98
Brice Videau <bvideau@anl.gov>
2022-03-07 18:20:41 -0600

    Added support for dumping linked structs.

---
+++ ze/gen_zet_structs_tracepoints.rb
@@ -0,0 +1,14 @@
+require_relative 'ze_model'
+require_relative 'gen_probe_base.rb'
+
+provider = :lttng_ust_zet_structs
+
+puts <<EOF
+#include "ze.h.include"
+EOF
+
+$zet_api["typedefs"].select { |t|
+  t.type.kind_of?(YAMLCAst::Struct) && (struct = $zet_api["structs"].find { |s| t.type.name == s.name }) && struct.members.first.name == "stype"
+}.each { |t|
+  $struct_tracepoint_lambda.call(provider, t.name)
+}
