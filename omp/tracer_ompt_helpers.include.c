fbaefc7eec2a913ea5d03d9cce75a0d06866ac83
Thomas Applencourt <tapplencourt@anl.gov>
2022-03-16 23:59:29 +0000

    More robust makefile

--- omp/tracer_ompt_helpers.include.c
+++ omp/tracer_ompt_helpers.include.c
@@ -1,10 +1,7 @@
-#include <stdio.h>
-
 int ompt_callback_control_tool_func(uint64_t command, uint64_t modifier, void *arg, const void *codeptr_ra) {
   tracepoint(lttng_ust_ompt, ompt_callback_control_tool, command, modifier, arg, codeptr_ra);
   return 0;
 }
-
 
 static void _ompt_finalize(ompt_data_t *tool_data) {
     (void) tool_data;
@@ -16,11 +13,15 @@ static int _ompt_initialize(ompt_function_lookup_t lookup,
 {
     (void) tool_data;
     (void) initial_device_num;
+
     ompt_set_callback_t ompt_set_callback = (ompt_set_callback_t) (uintptr_t) lookup("ompt_set_callback");
     if (ompt_set_callback == NULL)
         return 0;
+
     ompt_set_callback(ompt_callback_parallel_begin, (ompt_callback_t) (uintptr_t) ompt_callback_parallel_begin_func);
     ompt_set_callback(ompt_callback_parallel_end, (ompt_callback_t) (uintptr_t) ompt_callback_parallel_end_func);
+    ompt_set_callback(ompt_callback_target, (ompt_callback_t) (uintptr_t) ompt_callback_target_func);
+    ompt_set_callback(ompt_callback_target_data_op, (ompt_callback_t) (uintptr_t) ompt_callback_target_data_op_func); 
     return 1;
 }
 
