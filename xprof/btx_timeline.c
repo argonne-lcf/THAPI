d2d459beec7e345de01ef7c20e2b18b2acad1cd6
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-04-24 16:08:39 +0000

    split libXProf_la_ into libXTimeline_la_ and libXTally_la_

--- xprof/btx_timeline.c
+++ xprof/btx_timeline.c
@@ -8,16 +8,6 @@ BT_PLUGIN_MODULE();
 BT_PLUGIN(xprof);
 
 //~ ~ ~
-// Sink Tally
-//~ ~ ~
-
-BT_PLUGIN_SINK_COMPONENT_CLASS(tally, tally_dispatch_consume);
-
-BT_PLUGIN_SINK_COMPONENT_CLASS_INITIALIZE_METHOD(tally, tally_dispatch_initialize);
-BT_PLUGIN_SINK_COMPONENT_CLASS_FINALIZE_METHOD(tally, tally_dispatch_finalize);
-BT_PLUGIN_SINK_COMPONENT_CLASS_GRAPH_IS_CONFIGURED_METHOD(tally, tally_dispatch_graph_is_configured);
-
-//~ ~ ~
 // Sink Timeline
 //~ ~ ~
 
