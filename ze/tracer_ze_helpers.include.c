e4905a487e211ca388e1e2d2d140f6e591d2025c
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2023-05-24 20:11:58 +0000

    added gpu energy sampling. do_tracepoint not working

--- ze/tracer_ze_helpers.include.c
+++ ze/tracer_ze_helpers.include.c
@@ -750,11 +750,177 @@ static inline void _dump_memory_info(ze_command_list_handle_t hCommandList, cons
   }
 }
 
+////////////////////////////////////////////
+
+#define _ZE_ERROR_MSG(NAME,RES) {fprintf(stderr,"%s() failed at %d(%s): res=%x\n",(NAME),__LINE__,__FILE__,(RES));}
+#define _ZE_ERROR_MSG_NOTERMINATE(NAME,RES) {fprintf(stderr,"%s() error at %d(%s): res=%x\n",(NAME),__LINE__,__FILE__,(RES));}
+#define _ERROR_MSG(MSG) {perror((MSG)); fprintf(stderr,"errno=%d at %d(%s)",errno,__LINE__,__FILE__);}
+
+static int initialized = 0;
+
+#define MAX_N_DEVS  (8)
+#define MAX_N_PDOMS (8)
+
+static int selected_driver_id = 0;
+static ze_driver_handle_t selected_drvh;
+
+static uint32_t n_devhs;
+static ze_device_handle_t  devhs_cache[MAX_N_DEVS];
+static zes_pwr_handle_t   mainpwrh_per_dev[MAX_N_DEVS]; // main power domain associated with each device
+static int canreadpwrh_per_dev[MAX_N_DEVS];
+
+
+int zerGetNDevs()
+{
+  if(!initialized) return 0;
+  return n_devhs;
+}
+
+void  zerReadEnergy(int devid, uint64_t *ts_us, uint64_t *energy_uj)
+{
+  *ts_us = 0;
+  *energy_uj = 0;
+  if (!initialized) return;
+
+  if (canreadpwrh_per_dev[devid]) {
+    zes_power_energy_counter_t ecounter;
+    ZES_POWER_GET_ENERGY_COUNTER_PTR(mainpwrh_per_dev[devid], &ecounter);
+    *ts_us = ecounter.timestamp;
+    *energy_uj = ecounter.energy;
+  }
+}
+
+// return non-zero if failed to initialize
+int zerInit()
+{
+  ze_result_t res;
+
+  const char *e = getenv("ZES_ENABLE_SYSMAN");
+  if (!(e && e[0] == '1'))  {
+	fprintf(stderr,"ZES_ENABLE_SYSMAN needs to be set!\n");
+	return -1;
+  }
+
+#ifdef CALL_ZEINIT
+  res = zeInit(ZE_INIT_FLAG_GPU_ONLY);
+  if (res != ZE_RESULT_SUCCESS) {
+    _ZE_ERROR_MSG("zeInit", res);
+    return -1;
+  }
+#endif
+
+  /*
+   * detect drivers
+   */
+  uint32_t n_drvs = 0;
+  res = ZE_DRIVER_GET_PTR(&n_drvs, NULL);
+  if (res != ZE_RESULT_SUCCESS || n_drvs == 0) {
+	fprintf(stderr, "ERROR: No driver found!\n");
+	_ZE_ERROR_MSG("ZE_DRIVER_GET_PTR", res);
+	return -1;
+  }
+  ze_driver_handle_t *drvhs = (ze_driver_handle_t*)alloca(n_drvs*sizeof(ze_driver_handle_t));
+  printf("n_drvs=%d\n", n_drvs);
+
+  res = ZE_DRIVER_GET_PTR(&n_drvs, drvhs);
+  if (res != ZE_RESULT_SUCCESS) {
+    _ZE_ERROR_MSG("2nd ZE_DRIVER_GET_PTR", res);
+    return -1;
+  }
+
+  selected_drvh = drvhs[selected_driver_id]; // assume this is a deep-copy
+
+  /*
+   * detect devices on selected_drvh
+   */
+  n_devhs = 0;
+  res = ZE_DEVICE_GET_PTR(selected_drvh, &n_devhs, NULL);
+  if (res != ZE_RESULT_SUCCESS || n_devhs == 0) {
+	fprintf(stderr, "ERROR: No device found!\n");
+	_ZE_ERROR_MSG("ZE_DEVICE_GET_PTR", res);
+	return -1;
+  }
+  if (n_devhs >= MAX_N_DEVS) {
+	fprintf(stderr, "ERROR: %d devs are detected, which exceeds MAX_N_DEVS\n", n_devhs);
+	return -1;
+  }
+
+  res = ZE_DEVICE_GET_PTR(selected_drvh, &n_devhs, devhs_cache);
+  if (res != ZE_RESULT_SUCCESS) {
+	_ZE_ERROR_MSG("2nd ZE_DEVICE_GET_PTR", res);
+	return -1;
+  }
+
+  // iterate devices to find power domains associated with each device
+  for (uint32_t i=0; i<n_devhs; i++) {
+    canreadpwrh_per_dev[i] = 0;
+
+    ze_device_properties_t props = {0};
+    props.stype = ZE_STRUCTURE_TYPE_DEVICE_PROPERTIES;
+    props.pNext = NULL;
+    res = ZE_DEVICE_GET_PROPERTIES_PTR(devhs_cache[i], &props);
+    if (res != ZE_RESULT_SUCCESS) {
+      _ZE_ERROR_MSG("ZE_DEVICE_GET_PROPERTIES_PTR", res);
+      return -1;
+    }
+    if (props.type == ZE_DEVICE_TYPE_GPU) {
+      zes_device_handle_t smh = smh = (zes_device_handle_t)devhs_cache[i];
+      zes_pwr_handle_t pwrhs[MAX_N_PDOMS];
+      uint32_t npwrdoms = 0;
+
+      res = ZES_DEVICE_ENUM_POWER_DOMAINS_PTR(smh, &npwrdoms, NULL);
+      if (res != ZE_RESULT_SUCCESS) {
+	_ZE_ERROR_MSG("ZES_DEVICE_ENUM_POWER_DOMAINS_PTR", res);
+      } else {
+	if (npwrdoms > 0 && npwrdoms <= MAX_N_PDOMS) {
+	  res = ZES_DEVICE_ENUM_POWER_DOMAINS_PTR(smh, &npwrdoms, pwrhs);
+	  if (res != ZE_RESULT_SUCCESS) {
+	    _ZE_ERROR_MSG("ZES_DEVICE_ENUM_POWER_DOMAINS_PTR", res);
+	    return -1;
+					   }
+	  mainpwrh_per_dev[i] = pwrhs[0];
+	  canreadpwrh_per_dev[i] = 1;
+	  if(0) {
+	    zes_power_energy_counter_t ecounter;
+	    res = ZES_POWER_GET_ENERGY_COUNTER_PTR(mainpwrh_per_dev[i], &ecounter);
+	    if (res != ZE_RESULT_SUCCESS)  _ZE_ERROR_MSG_NOTERMINATE("ZES_POWER_GET_ENERGY_COUNTER_PTR", res);
+	    printf("%lu us  %lu uj\n", ecounter.timestamp, ecounter.energy);
+	  }
+	} else {
+	  fprintf(stderr, "Warning: npwrdoms=%d\n", npwrdoms);
+	}
+      }
+    } else {
+      fprintf(stderr, "Warning: dev%d is not a GPU!\n", i);
+    }
+  }
+
+  initialized = 1;
+  return 0;
+}
+
+/////////////////////////////////
+
+
+static void thapi_sampling_energy() {
+  uint64_t ts_us;
+  uint64_t energy_uj;
+
+  for (int i=0; i<zerGetNDevs(); i++) {
+    zerReadEnergy(i, &ts_us, &energy_uj);
+    do_tracepoint(lttng_ust_ze_sampling, gpu_energy,
+		  (ze_device_handle_t)devhs_cache[i],
+		  (uint64_t)energy_uj);
+    // printf("thapi_sampling_energy i=%d energy_uj=%lu\n", i, energy_uj);
+  }
+}
+
+
 static void _load_tracer(void) {
   char *s = NULL;
   void *handle = NULL;
   int verbose = 0;
-
+  struct timespec interval;
   thapi_sampling_init();
 
   s = getenv("LTTNG_UST_ZE_LIBZE_LOADER");
@@ -800,6 +966,14 @@ static void _load_tracer(void) {
     else if (verbose)
       fprintf(stderr, "Warning: LTTNG_UST_ZE_PARANOID_DRIFT not activated without LTTNG_UST_ZE_PROFILE\n");
   }
+
+  if (getenv("LTTNG_UST_SAMPLING_ENERGY")) {
+    zerInit();
+    interval.tv_sec = 0;
+    interval.tv_nsec = 50000000;
+    thapi_register_sampling(&thapi_sampling_energy, &interval);
+  }
+
   if (_do_profile)
     atexit(&_lib_cleanup);
 }
