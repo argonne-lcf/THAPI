fdd844624f16b10c932e81e513ab2613fcd5640a
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-10-30 18:24:52 +0000

    Suggestions

--- hip/btx_hipinterval_callbacks.cpp
+++ hip/btx_hipinterval_callbacks.cpp
@@ -1,57 +1,74 @@
 #include <metababel/metababel.h>
 
-#include <unordered_map>
-#include <iostream>
 #include <assert.h>
+#include <iostream>
 #include <regex>
 #include <tuple>
+#include <string>
+#include <unordered_map>
 
 #include <hip.h.include>
-
 #include "xprof_utils.hpp"
-#include "btx_hipinterval_callbacks.hpp"
 
 
-void send_message_lttng_host(void *btx_handle, void *usr_data, int64_t ts, const char *event_class_name, 
-                             const char *hostname, int64_t vpid, uint64_t vtid, int64_t err){
+struct data_s {
+  std::unordered_map<hpt_function_name_t, int64_t> dispatch;
+};
+
+typedef struct data_s data_t;
+
+std::string strip_event_class_name(const char *str) {
+  std::string temp(str);
+  std::smatch match;
+  std::regex_search(temp, match, std::regex(":(.*?)_?(?:entry|exit)?$"));
+
+  // The entire match is hold in the first item, sub_expressions 
+  // (parentheses delimited groups) are stored after.
+  assert( match.size() > 1 && "Event_class_name not matching regex.");
+
+  return match[1].str();
+}
+
+static void send_host_message(void *btx_handle, void *usr_data, int64_t ts,
+                              const char *event_class_name, const char *hostname, int64_t vpid,
+                              uint64_t vtid, bool err) {
+
   auto *data = static_cast<data_t *>(usr_data);
 
   std::string event_class_name_striped = strip_event_class_name(event_class_name);
-  int64_t _start = data->dispatch.at(hpt_fn_t(hostname, vpid, vtid, event_class_name_striped));
+  const int64_t _start = data->dispatch.at({hostname, vpid, vtid, event_class_name_striped});
 
-  btx_push_message_lttng_host(btx_handle, hostname, vpid, vtid, _start, 
-                              BACKEND_HIP, event_class_name_striped.c_str(), (ts - _start), err);                     
+  btx_push_message_lttng_host(btx_handle, hostname, vpid, vtid, _start, BACKEND_HIP,
+                              event_class_name_striped.c_str(), (ts - _start), err);
 }
 
-void btx_initialize_component(void *btx_handle, void **usr_data) {
-  *usr_data = new data_t;
-}
+void btx_initialize_component(void *btx_handle, void **usr_data) { *usr_data = new data_t; }
 
 void btx_finalize_component(void *btx_handle, void *usr_data) {
-  auto *data = static_cast<data_t *>(usr_data);
-  delete data;
+  delete static_cast<data_t *>(usr_data);
 }
 
-static void entries_callback(void *btx_handle, void *usr_data, int64_t ts, const char *event_class_name, 
-                             const char *hostname, int64_t vpid, uint64_t vtid) {
-  data_t *data = (data_t *)usr_data;
-  data->dispatch[hpt_fn_t(hostname, vpid, vtid, strip_event_class_name(event_class_name))] = ts;
+static void entries_callback(void *btx_handle, void *usr_data, int64_t ts,
+                             const char *event_class_name, const char *hostname, int64_t vpid,
+                             uint64_t vtid) {
+
+  ((data_t *)usr_data)->dispatch[{hostname, vpid, vtid, strip_event_class_name(event_class_name)}] = ts;
 }
 
-static void exits_callback_hipError_absent(
-  void *btx_handle, void *usr_data, int64_t ts, const char *event_class_name, 
-  const char *hostname, int64_t vpid, uint64_t vtid) {
+static void exits_callback_hipError_absent(void *btx_handle, void *usr_data, int64_t ts,
+                                           const char *event_class_name, const char *hostname,
+                                           int64_t vpid, uint64_t vtid) {
 
-  send_message_lttng_host(btx_handle, usr_data, ts, event_class_name, hostname, vpid, vtid, 0);
+  send_host_message(btx_handle, usr_data, ts, event_class_name, hostname, vpid, vtid, false);
 }
 
-static void exits_callback_hipError_present(
-  void *btx_handle, void *usr_data, int64_t ts, const char *event_class_name, 
-  const char *hostname, int64_t vpid, uint64_t vtid, int64_t hipResult) {
+static void exits_callback_hipError_present(void *btx_handle, void *usr_data, int64_t ts,
+                                            const char *event_class_name, const char *hostname,
+                                            int64_t vpid, uint64_t vtid, int64_t hipResult) {
 
   // Not an Error (hipResult == hipErrorNotReady)
-  int64_t err = hipResult != 0 ? (hipResult == hipErrorNotReady ? 0 : hipResult) : 0; 
-  send_message_lttng_host(btx_handle, usr_data, ts, event_class_name, hostname, vpid, vtid, err);
+  bool err = (hipResult != 0) && (hipResult != hipErrorNotReady);
+  send_host_message(btx_handle, usr_data, ts, event_class_name, hostname, vpid, vtid, err);
 }
 
 void btx_register_usr_callbacks(void *btx_handle) {
