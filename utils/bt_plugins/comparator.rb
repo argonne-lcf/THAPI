4e9422c3102f678928a509aa9e472b971243c5ca
Thomas Applencourt <apl@anl.gov>
2024-01-12 21:03:16 +0000

    Fix CI?

--- utils/bt_plugins/comparator.rb
+++ utils/bt_plugins/comparator.rb
@@ -8,26 +8,23 @@ class Comparator
     # and compare events fields
     # If the number of message consumed by message iterator are differnts
     # an error is raised
-    stack_messages = [[], []]
-
-    @message_iterators.each_with_index { |message_iterator, i|
-      message_iterator.next_messages.each { |m|
-        stack_messages[i] << m.event if m.type == :BT_MESSAGE_TYPE_EVENT
-      }
-    }
-
-    stack_messages.transpose.each { |i, j|
-      %w[payload specific_context common_context].each { |tf|
-        i_value = i.send("get_#{tf}_field")
-        i_value = i_value.value if i_value
-        j_value = j.send("get_#{tf}_field")
-        j_value = j_value.value if j_value
-        unless i_value == j_value
-          pp({ in0: i_value, in1: j_value })
-          raise "Traces for #{tf} fields are different!"
+    stack_messages = @message_iterators.map do |message_iterator|
+      message_iterator.next_messages.filter_map do |m|
+        m.event if m.type == :BT_MESSAGE_TYPE_EVENT
+      end.flat_map do |e|
+        %w[payload specific_context common_context].filter_map do |tf|
+          field = e.send("get_#{tf}_field")
+          field.value if field
         end
-      }
-    }
+      end
+    end
+
+    stack_messages.transpose.each do |i_value, j_value|
+      unless i_value == j_value
+        pp({ in0: i_value, in1: j_value })
+        raise "#{i_value} != #{j_value}!"
+      end
+    end
   end
 
   def initialize_method(self_component, _configuration, _params, _data)
@@ -36,18 +33,18 @@ class Comparator
   end
 
   def graph_is_configured_method(self_component)
-    @message_iterators = self_component.get_input_port_count.times.map { |i|
+    @message_iterators = self_component.get_input_port_count.times.map do |i|
       p = self_component.get_input_port_by_index(i)
       self_component.create_message_iterator(p)
-    }
+    end
   end
 
   def create_component_class
     component_class = BT2::BTComponentClass::Sink.new(name: 'comparator',
-                                                      consume_method: lambda(&method(:consume_method)))
+                                                      consume_method: method(:consume_method))
 
-    component_class.initialize_method = lambda(&method(:initialize_method))
-    component_class.graph_is_configured_method = lambda(&method(:graph_is_configured_method))
+    component_class.initialize_method = method(:initialize_method)
+    component_class.graph_is_configured_method = method(:graph_is_configured_method)
     component_class
   end
 end
