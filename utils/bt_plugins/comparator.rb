3bd48185a44dd13fd7b0e29440ffe69b562e8fc1
Thomas Applencourt <apl@anl.gov>
2024-01-13 00:35:44 +0000

    Fix CI (and tests...)

--- utils/bt_plugins/comparator.rb
+++ utils/bt_plugins/comparator.rb
@@ -4,10 +4,10 @@ require 'optparse'
 
 class Comparator
   def consume_method(_self_component)
-    # This function will comsume message for all the message iterator
-    @message_iterators.delete_if do |message_iterator, i|
+    # This function will consume message for all the message iterator
+    @message_iterators.delete_if do |message_iterator|
       message_iterator.next_messages.each do |m|
-        @stack_messages[i] << m.event if m.type == :BT_MESSAGE_TYPE_EVENT
+        @stack_messages[message_iterator] << m.event if m.type == :BT_MESSAGE_TYPE_EVENT
       end
     rescue StopIteration
       true
@@ -15,17 +15,21 @@ class Comparator
       false
     end
 
-    raise StopIteration if @message_iterators.empty?
+    return unless @message_iterators.empty?
+
+    verify_messages
+    @stack_messages = nil
+    raise StopIteration
   end
 
   def initialize_method(self_component, _configuration, _params, _data)
-    @stack_messages = [[], []]
+    @stack_messages = Hash.new{ |h,k| h[k] = [] }
     self_component.add_input_port('in0')
     self_component.add_input_port('in1')
   end
 
-  def finalize_method(_self_component)
-    @stack_messages.transpose.each do |i, j|
+  def verify_messages
+    @stack_messages.values.transpose.each do |i, j|
       %w[payload specific_context common_context].each do |tf|
         i_value = i.send("get_#{tf}_field")
         i_value = i_value.value if i_value
@@ -42,7 +46,7 @@ class Comparator
   def graph_is_configured_method(self_component)
     @message_iterators = self_component.get_input_port_count.times.map do |i|
       p = self_component.get_input_port_by_index(i)
-      [self_component.create_message_iterator(p), i]
+      self_component.create_message_iterator(p)
     end
   end
 
@@ -51,9 +55,7 @@ class Comparator
                                                       consume_method: lambda(&method(:consume_method)))
 
     component_class.initialize_method = lambda(&method(:initialize_method))
-    component_class.finalize_method = lambda(&method(:finalize_method))
     component_class.graph_is_configured_method = lambda(&method(:graph_is_configured_method))
     component_class
   end
-
 end
