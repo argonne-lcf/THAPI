4ebbb2de29dd8307cd9cd8a98ab31a5a731e6b11
Bryce Allen <bdallen@uchicago.edu>
2024-02-09 09:35:19 -0500

    cuda btx: remove unused THAPI_FATAL macros

--- cuda/btx_cudainterval_callbacks.hpp
+++ cuda/btx_cudainterval_callbacks.hpp
@@ -16,19 +16,3 @@ using context_stack_t = std::stack<CUcontext>;
 inline bool cuResultIsError(CUresult& cuResult) {
   return (cuResult != CUDA_SUCCESS) && (cuResult != CUDA_ERROR_NOT_READY);
 }
-
-#define THAPI_FATAL(msg) __thapi_fatal(msg, __FILE__, __LINE__)
-#define THAPI_FATAL_HPT(hpt, msg) __thapi_fatal_hpt(hpt, msg, __FILE__, __LINE__)
-
-inline void __thapi_fatal(const char *msg, const char *fname, int line) {
-  std::cerr << "ERROR at " << fname << " " << line << ": " << msg << std::endl;
-  abort();
-}
-
-inline void __thapi_fatal_hpt(hpt_t hpt, const char *msg, const char *fname, int line) {
-  std::cerr << "ERROR at " << fname << " " << line << ": " << msg
-            << " {" << std::get<0>(hpt) << ", " << std::get<1>(hpt)
-            << ", " << std::get<2>(hpt) << "}" << std::endl;
-  abort();
-}
-
