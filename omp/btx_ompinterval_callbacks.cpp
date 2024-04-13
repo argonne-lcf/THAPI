eb9f7453851155961f0005e082e317a6f703e56e
Bryce Allen <bdallen@uchicago.edu>
2024-04-12 19:01:20 -0500

    omp: refactor duplicate code

--- omp/btx_ompinterval_callbacks.cpp
+++ omp/btx_ompinterval_callbacks.cpp
@@ -1,10 +1,5 @@
 #include <metababel/metababel.h>
 
-#include <assert.h>
-#include <iostream>
-#include <regex>
-#include <string>
-#include <tuple>
 #include <unordered_map>
 
 #include "ompt.h.include"
@@ -12,14 +7,14 @@
 #include "xprof_utils.hpp"
 
 struct data_s {
-  // EntryState entry_state;
   std::unordered_map<hpt_function_name_t, uint64_t> host_start;
 };
 
 typedef struct data_s data_t;
 
 static const char *
-ompt_callback_target_data_op_intel_name(ompt_target_data_op_t optype) {
+ompt_callback_target_data_op_name(ompt_target_data_op_t optype,
+                                  const char *fallback_name) {
   switch (optype) {
   case ompt_target_data_alloc:
     return "ompt_target_data_alloc";
@@ -42,37 +37,11 @@ ompt_callback_target_data_op_intel_name(ompt_target_data_op_t optype) {
   case ompt_target_data_delete_async:
     return "ompt_target_data_delete_async";
   }
-  return "ompt_target_data_op_intel";
+  return fallback_name;
 }
 
-static const char *
-ompt_callback_target_data_op_emi_name(ompt_target_data_op_t optype) {
-  switch (optype) {
-  case ompt_target_data_alloc:
-    return "ompt_target_data_alloc";
-  case ompt_target_data_transfer_to_device:
-    return "ompt_target_data_transfer_to_device";
-  case ompt_target_data_transfer_from_device:
-    return "ompt_target_data_transfer_from_device";
-  case ompt_target_data_delete:
-    return "ompt_target_data_delete";
-  case ompt_target_data_associate:
-    return "ompt_target_data_associate";
-  case ompt_target_data_disassociate:
-    return "ompt_target_data_disassociate";
-  case ompt_target_data_alloc_async:
-    return "ompt_target_data_alloc_async";
-  case ompt_target_data_transfer_to_device_async:
-    return "ompt_target_data_transfer_to_device_async";
-  case ompt_target_data_transfer_from_device_async:
-    return "ompt_target_data_transfer_from_device_async";
-  case ompt_target_data_delete_async:
-    return "ompt_target_data_delete_async";
-  }
-  return "ompt_target_data_op_emi";
-}
-
-static const char *ompt_callback_target_name(ompt_target_t kind) {
+static const char *ompt_callback_target_name(ompt_target_t kind,
+                                             const char *fallback_name) {
   switch (kind) {
   case ompt_target:
     return "ompt_target";
@@ -91,7 +60,7 @@ static const char *ompt_callback_target_name(ompt_target_t kind) {
   case ompt_target_update_nowait:
     return "ompt_target_update_nowait";
   }
-  return "ompt_target";
+  return fallback_name;
 }
 
 static void btx_initialize_component(void **usr_data) {
@@ -102,22 +71,30 @@ static void btx_finalize_component(void *usr_data) {
   delete static_cast<data_t *>(usr_data);
 }
 
+static void _target_callback(void *btx_handle, void *usr_data, int64_t ts,
+                             const char *hostname, int64_t vpid, uint64_t vtid,
+                             ompt_scope_endpoint_t endpoint,
+                             const char *op_name) {
+  auto state = static_cast<data_t *>(usr_data);
+  if (endpoint == ompt_scope_begin) {
+    state->host_start[{hostname, vpid, vtid, op_name}] = ts;
+  } else if (endpoint == ompt_scope_end) {
+    // TODO: detect error?
+    const bool err = false;
+    auto start_ts = state->host_start[{hostname, vpid, vtid, op_name}];
+    btx_push_message_lttng_host(btx_handle, hostname, vpid, vtid, start_ts,
+                                BACKEND_OMP, op_name, (ts - start_ts), err);
+  }
+}
+
 static void ompt_callback_target_callback(
     void *btx_handle, void *usr_data, int64_t ts, const char *hostname,
     int64_t vpid, uint64_t vtid, ompt_target_t kind,
     ompt_scope_endpoint_t endpoint, int device_num, ompt_data_t *task_data,
     ompt_id_t target_id, void *codeptr_ra) {
-  auto state = static_cast<data_t *>(usr_data);
-  const char *_name = ompt_callback_target_name(kind);
-  if (endpoint == ompt_scope_begin) {
-    state->host_start[{hostname, vpid, vtid, _name}] = ts;
-  } else if (endpoint == ompt_scope_end) {
-    // TODO: detect error?
-    const bool err = false;
-    auto start_ts = state->host_start[{hostname, vpid, vtid, _name}];
-    btx_push_message_lttng_host(btx_handle, hostname, vpid, vtid, start_ts,
-                                BACKEND_OMP, _name, (ts - start_ts), err);
-  }
+  const char *op_name = ompt_callback_target_name(kind, "ompt_target");
+  _target_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, endpoint,
+                   op_name);
 }
 
 static void ompt_callback_target_emi_callback(
@@ -125,16 +102,28 @@ static void ompt_callback_target_emi_callback(
     int64_t vpid, uint64_t vtid, ompt_target_t kind,
     ompt_scope_endpoint_t endpoint, int device_num, ompt_data_t *task_data,
     ompt_data_t *target_task_data, ompt_data_t *target_data, void *codeptr_ra) {
+  const char *op_name = ompt_callback_target_name(kind, "ompt_target_emi");
+  _target_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, endpoint,
+                   op_name);
+}
+
+static void _data_op_callback(void *btx_handle, void *usr_data, int64_t ts,
+                              const char *hostname, int64_t vpid, uint64_t vtid,
+                              ompt_scope_endpoint_t endpoint, size_t bytes,
+                              const char *op_name) {
   auto state = static_cast<data_t *>(usr_data);
-  const char *_name = ompt_callback_target_name(kind);
   if (endpoint == ompt_scope_begin) {
-    state->host_start[{hostname, vpid, vtid, _name}] = ts;
+    state->host_start[{hostname, vpid, vtid, op_name}] = ts;
   } else if (endpoint == ompt_scope_end) {
     // TODO: detect error?
     const bool err = false;
-    auto start_ts = state->host_start[{hostname, vpid, vtid, _name}];
+    auto start_ts = state->host_start[{hostname, vpid, vtid, op_name}];
     btx_push_message_lttng_host(btx_handle, hostname, vpid, vtid, start_ts,
-                                BACKEND_OMP, _name, (ts - start_ts), err);
+                                BACKEND_OMP, op_name, (ts - start_ts), err);
+    if (strcmp(op_name, "ompt_target_data_alloc") != 0) {
+      btx_push_message_lttng_traffic(btx_handle, hostname, vpid, vtid, start_ts,
+                                     BACKEND_OMP, op_name, bytes);
+    }
   }
 }
 
@@ -144,21 +133,10 @@ static void ompt_callback_target_data_op_intel_callback(
     ompt_id_t target_id, ompt_id_t host_op_id, ompt_target_data_op_t optype,
     void *src_addr, int src_device_num, void *dest_addr, int dest_device_num,
     size_t bytes, void *codeptr_ra) {
-  auto state = static_cast<data_t *>(usr_data);
-  const char *_name = ompt_callback_target_data_op_intel_name(optype);
-  if (endpoint == ompt_scope_begin) {
-    state->host_start[{hostname, vpid, vtid, _name}] = ts;
-  } else if (endpoint == ompt_scope_end) {
-    // TODO: detect error?
-    const bool err = false;
-    auto start_ts = state->host_start[{hostname, vpid, vtid, _name}];
-    btx_push_message_lttng_host(btx_handle, hostname, vpid, vtid, start_ts,
-                                BACKEND_OMP, _name, (ts - start_ts), err);
-    if (strcmp(_name, "ompt_target_data_alloc") != 0) {
-      btx_push_message_lttng_traffic(btx_handle, hostname, vpid, vtid, start_ts,
-                                     BACKEND_OMP, _name, bytes);
-    }
-  }
+  const char *op_name =
+      ompt_callback_target_data_op_name(optype, "ompt_target_data_op_intel");
+  _data_op_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, endpoint,
+                    bytes, op_name);
 }
 
 static void ompt_callback_target_data_op_emi_callback(
@@ -168,31 +146,36 @@ static void ompt_callback_target_data_op_emi_callback(
     ompt_id_t *host_op_id, ompt_target_data_op_t optype, void *src_addr,
     int src_device_num, void *dest_addr, int dest_device_num, size_t bytes,
     void *codeptr_ra) {
-  auto state = static_cast<data_t *>(usr_data);
-  const char *_name = ompt_callback_target_data_op_emi_name(optype);
-  if (endpoint == ompt_scope_begin) {
-    state->host_start[{hostname, vpid, vtid, _name}] = ts;
-  } else if (endpoint == ompt_scope_end) {
-    // TODO: detect error?
-    const bool err = false;
-    auto start_ts = state->host_start[{hostname, vpid, vtid, _name}];
-    btx_push_message_lttng_host(btx_handle, hostname, vpid, vtid, start_ts,
-                                BACKEND_OMP, _name, (ts - start_ts), err);
-    if (strcmp(_name, "ompt_target_data_alloc") != 0) {
-      btx_push_message_lttng_traffic(btx_handle, hostname, vpid, vtid, start_ts,
-                                     BACKEND_OMP, _name, bytes);
-    }
-  }
+  const char *op_name =
+      ompt_callback_target_data_op_name(optype, "ompt_target_data_op_emi");
+  _data_op_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, endpoint,
+                    bytes, op_name);
 }
 
 static void ompt_callback_target_submit_callback(
     void *btx_handle, void *usr_data, int64_t ts, const char *hostname,
     int64_t vpid, uint64_t vtid, ompt_id_t target_id, ompt_id_t host_op_id,
     unsigned int requested_num_teams) {
-  const char *_name = "ompt_target_submit";
+  const char *op_name = "ompt_target_submit";
   const bool err = false;
   btx_push_message_lttng_host(btx_handle, hostname, vpid, vtid, ts, BACKEND_OMP,
-                              _name, 0, err);
+                              op_name, 0, err);
+}
+
+static void _submit_callback(void *btx_handle, void *usr_data, int64_t ts,
+                             const char *hostname, int64_t vpid, uint64_t vtid,
+                             ompt_scope_endpoint_t endpoint,
+                             const char *op_name) {
+  auto state = static_cast<data_t *>(usr_data);
+  if (endpoint == ompt_scope_begin) {
+    state->host_start[{hostname, vpid, vtid, op_name}] = ts;
+  } else if (endpoint == ompt_scope_end) {
+    // TODO: detect error?
+    const bool err = false;
+    auto start_ts = state->host_start[{hostname, vpid, vtid, op_name}];
+    btx_push_message_lttng_host(btx_handle, hostname, vpid, vtid, start_ts,
+                                BACKEND_OMP, op_name, (ts - start_ts), err);
+  }
 }
 
 static void ompt_callback_target_submit_intel_callback(
@@ -200,17 +183,8 @@ static void ompt_callback_target_submit_intel_callback(
     int64_t vpid, uint64_t vtid, ompt_scope_endpoint_t endpoint,
     ompt_id_t target_id, ompt_id_t host_op_id,
     unsigned int requested_num_teams) {
-  auto state = static_cast<data_t *>(usr_data);
-  const char *_name = "ompt_target_submit_intel";
-  if (endpoint == ompt_scope_begin) {
-    state->host_start[{hostname, vpid, vtid, _name}] = ts;
-  } else if (endpoint == ompt_scope_end) {
-    // TODO: detect error?
-    const bool err = false;
-    auto start_ts = state->host_start[{hostname, vpid, vtid, _name}];
-    btx_push_message_lttng_host(btx_handle, hostname, vpid, vtid, start_ts,
-                                BACKEND_OMP, _name, (ts - start_ts), err);
-  }
+  _submit_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, endpoint,
+                   "ompt_target_submit_intel");
 }
 
 static void ompt_callback_target_submit_emi_callback(
@@ -218,17 +192,8 @@ static void ompt_callback_target_submit_emi_callback(
     int64_t vpid, uint64_t vtid, ompt_scope_endpoint_t endpoint,
     ompt_data_t *target_data, ompt_id_t *host_op_id,
     unsigned int requested_num_teams) {
-  auto state = static_cast<data_t *>(usr_data);
-  const char *_name = "ompt_target_submit_emi";
-  if (endpoint == ompt_scope_begin) {
-    state->host_start[{hostname, vpid, vtid, _name}] = ts;
-  } else if (endpoint == ompt_scope_end) {
-    // TODO: detect error?
-    const bool err = false;
-    auto start_ts = state->host_start[{hostname, vpid, vtid, _name}];
-    btx_push_message_lttng_host(btx_handle, hostname, vpid, vtid, start_ts,
-                                BACKEND_OMP, _name, (ts - start_ts), err);
-  }
+  _submit_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, endpoint,
+                   "ompt_target_submit_emi");
 }
 
 #define REGISTER_ASSOCIATED_CALLBACK(base_name)                                \
