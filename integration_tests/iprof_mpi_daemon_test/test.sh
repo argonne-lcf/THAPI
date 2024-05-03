f26c660bcb243c1c11eb303d375f15de5b4b5a3f
Thomas Applencourt <TApplencourt@users.noreply.github.com>
2024-05-03 15:25:49 -0500

    Update test.sh (#221)

--- integration_tests/iprof_mpi_daemon_test/test.sh
+++ integration_tests/iprof_mpi_daemon_test/test.sh
@@ -11,7 +11,6 @@ RT_SIGNAL_FINISH=$((SIGRTMIN + 3))
 
 # Initialize a variable to track signal reception
 SIGNAL_RECEIVED="false"
-
 # Signal handler for capturing signals
 handle_signal() {
     echo "Received signal $1 from mpi_daemon"
@@ -31,36 +30,27 @@ wait_for_signal() {
     done
 }
 
-# Get the PID of this script
-PARENT_PID=$$
-
-# Start mpi_daemon in the background
-./mpi_daemon $PARENT_PID &
-DAEMON_PID=$!
-
 # Function to send signals, using adjusted SIGRTMIN corresponding to MPI signal daemon defines
 send_signal_blocking() {
     kill -$1 $DAEMON_PID
     wait_for_signal
 }
 
+# Get the PID of this script
+PARENT_PID=$$
+# Start mpi_daemon in the background
+./mpi_daemon $PARENT_PID &
+DAEMON_PID=$!
 # Wait for daemon to be ready
 wait_for_signal
-
 # Send signals to mpi_daemon to test synchronization
 send_signal_blocking $RT_SIGNAL_LOCAL_BARRIER
-
 send_signal_blocking $RT_SIGNAL_GLOBAL_BARRIER
-
 # Run mpi_hello_world
 ./mpi_hello_world
-
 # Final synchronization after mpi_hello_world execution
 send_signal_blocking $RT_SIGNAL_LOCAL_BARRIER
-
 send_signal_blocking $RT_SIGNAL_GLOBAL_BARRIER
-
 # Signal to terminate the mpi_daemon
 send_signal_blocking $RT_SIGNAL_FINISH
-
 wait $DAEMON_PID  # Ensure daemon exits cleanly
