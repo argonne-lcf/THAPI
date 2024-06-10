f5477a0b276700946cda8f61308f79389504d0b4
Thomas Applencourt <apl@anl.gov>
2024-06-10 22:34:00 +0000

    Hello world working

--- mpi/gen_mpi.rb
+++ mpi/gen_mpi.rb
@@ -1,48 +1,62 @@
 require_relative 'mpi_model'
 
-puts <<EOF
-#include <stdint.h>
-#include <stddef.h>
-#include <mpi.h>
-#include "mpi_tracepoints.h"
+puts <<~EOF
+  #include <mpi.h>
+  #include "mpi_tracepoints.h"
 EOF
 
-
-common_block = lambda { |c, provider|
+def common_block(c, provider)
   params = c.parameters.collect(&:name)
-  tp_params = c.parameters.collect { |p|
-    if p.type.kind_of?(YAMLCAst::Pointer) && p.type.type.kind_of?(YAMLCAst::Function)
-      "(void *)(intptr_t)" + p.name
+  tp_params = c.parameters.collect do |p|
+    if p.type.is_a?(YAMLCAst::Pointer) && p.type.type.is_a?(YAMLCAst::Function)
+      '(void *)(intptr_t)' + p.name
     else
       p.name
     end
-  }
+  end
   tracepoint_params = c.tracepoint_parameters.collect(&:name)
-  c.tracepoint_parameters.each { |p|
+  c.tracepoint_parameters.each do |p|
     puts "  #{p.type} #{p.name};"
-  }
-  c.tracepoint_parameters.each { |p|
+  end
+  c.tracepoint_parameters.each do |p|
     puts p.init unless p.after?
-  }
+  end
   tracepoint_params = c.tracepoint_parameters.reject { |p| p.after? }.collect(&:name)
   puts <<EOF
-  tracepoint(#{provider}, #{c.name.gsub(/_func\z/,'')}, #{(tp_params+tracepoint_params).join(", ")});
+  tracepoint(#{provider}, #{c.name}_#{START}, #{(tp_params + tracepoint_params).join(', ')});
 EOF
-}
+  tracepoint_params = c.tracepoint_parameters.collect(&:name)
 
-normal_wrapper = lambda { |c, provider|
+  if c.has_return_type?
+    puts <<EOF
+  #{c.type} _retval;
+  _retval = P#{c.name}(#{params.join(', ')});
+EOF
+  else
+    puts "  P#{c.name}(#{params.join(', ')});"
+  end
+  c.tracepoint_parameters.each do |p|
+    puts p.init if p.after?
+  end
+  tp_params.push '_retval' if c.has_return_type?
   puts <<EOF
-static #{c.decl} {
+  tracepoint(#{provider}, #{c.name}_#{STOP}, #{(tp_params + tracepoint_params).join(', ')});
 EOF
-  common_block.call(c, provider)
-  puts <<EOF
-}
+  puts '  return _retval;' if c.has_return_type?
+end
 
-EOF
-}
+def normal_wrapper(c, provider)
+  puts <<~EOF
+    #{c.decl} {
+  EOF
+  common_block(c, provider)
+  puts <<~EOF
+    }
 
-$ompt_commands.each { |c|
-  normal_wrapper.call(c, :lttng_ust_mpi)
-}
+  EOF
+end
 
-puts File::read("tracer_mpi_helpers.include.c")
+$mpi_commands.each do |c|
+  next if c.name.start_with?("PMPI")
+  normal_wrapper(c, :lttng_ust_mpi)
+end
--- mpi/include/mpi.h
+++ mpi/include/mpi.h
@@ -1,1 +1,2 @@
 int MPI_Init(int *argc, char ***argv);
+int PMPI_Init(int *argc, char ***argv);
