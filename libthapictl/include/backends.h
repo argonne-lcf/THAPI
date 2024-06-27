803e64f754c24fc05f8f80f3b8b06f1a794f589d
Bryce Allen <bdallen@uchicago.edu>
2024-06-27 07:37:59 -0500

    add opencl fn to header

--- libthapictl/include/backends.h
+++ libthapictl/include/backends.h
@@ -5,3 +5,7 @@
 void thapi_cuda_init(struct lttng_handle *h, const char *channel_name);
 void thapi_cuda_enable_tracing_events(struct lttng_handle *h, const char *channel_name);
 void thapi_cuda_disable_tracing_events(struct lttng_handle *h, const char *channel_name);
+
+void thapi_opencl_init(struct lttng_handle *h, const char *channel_name);
+void thapi_opencl_enable_tracing_events(struct lttng_handle *h, const char *channel_name);
+void thapi_opencl_disable_tracing_events(struct lttng_handle *h, const char *channel_name);
