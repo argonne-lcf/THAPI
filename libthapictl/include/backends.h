23f280f013c10ee6509dba73b2ea73329f59f440
Bryce Allen <bdallen@uchicago.edu>
2024-06-26 19:47:00 -0500

    missing header

---
+++ libthapictl/include/backends.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <lttng/lttng.h>
+
+void thapi_cuda_init(struct lttng_handle *h, const char *channel_name);
+void thapi_cuda_enable_tracing_events(struct lttng_handle *h, const char *channel_name);
+void thapi_cuda_disable_tracing_events(struct lttng_handle *h, const char *channel_name);
