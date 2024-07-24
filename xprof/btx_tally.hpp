153ada26307e04d637df6bcc35611a2ddeee3adc
Thomas Applencourt <tapplencourt@anl.gov>
2024-07-24 15:29:13 -0500

    Fix tally (#269)
    * fix
    
    * working?
    
    * working
    
    * old rubocop
    
    * divide per 2 memory
    
    * consistancy
    
    ---------
    
    Co-authored-by: Thomas Applencourt <apl@anl.gov>
    Co-authored-by: Thomas Applencourt <applenco@aurora-uan-0012.hostmgmt.cm.aurora.alcf.anl.gov>
    Co-authored-by: Thomas Applencourt <applenco@aurora-uan-0009.hostmgmt.cm.aurora.alcf.anl.gov>

--- xprof/btx_tally.hpp
+++ xprof/btx_tally.hpp
@@ -41,9 +41,9 @@ public:
   void update_max_size(std::array<long, nfields> &m) {
     const auto current_string = to_string();
     for (auto i = 0UL; i < m.size(); i++)
-        m[i] = std::max(m[i], static_cast<long>(current_string[i].size()));
-    }
- 
+      m[i] = std::max(m[i], static_cast<long>(current_string[i].size()));
+  }
+
 private:
   // Pure virtual function
   virtual const std::array<std::string, nfields> to_string() = 0;
@@ -139,28 +139,40 @@ auto aggregate_by_name(std::unordered_map<std::tuple<T...>, TC> &m) {
 /*
 EXAMPLE:
   input   umap{
-                ("iris01",232,789,"getDeviceInfo") : CoreTime,
-                ("iris02",123,890,"getDeviceInfo") : CoreTime,
-                ("iris02",890,890,"getDeviceInfo") : CoreTime
+                ("iris01",0,0,"getDeviceInfo") : CoreTime,
+                ("iris01",0,1,"getDeviceInfo") : CoreTime,
+                ("iris01",1,0,"getDeviceInfo") : CoreTime
           }
-  output  tuple{ set {"iris01","iris02"}, set{232,123,890}, set{789,890}, set{"getDeviceInfo"} }
+  output  vector<size_t>{1, 2, 3}
 */
-template <class... K,  size_t... I>
-void add_to_set(std::tuple<std::set<K>...> &s,
-                std::tuple<K...> t, std::index_sequence<I...>) {
-  (std::get<I>(s).insert(std::get<I>(t)), ...);
+
+// Base case to stop recursion
+inline void _get_uniq_tally(std::set<std::tuple<>> &, std::vector<size_t> &) {}
+// General recursive case
+template <class... T> void _get_uniq_tally(std::set<std::tuple<T...>> &s, std::vector<size_t> &v) {
+  v.push_back(s.size());
+  typedef decltype(make_tuple_without_last(std::declval<std::tuple<T...>>())) Minusone;
+  std::set<Minusone> s_reduced;
+  for (auto &t : s)
+    s_reduced.insert(make_tuple_without_last(t));
+  _get_uniq_tally(s_reduced, v);
+}
+// Exposed function
+template <class... T, class K> auto get_uniq_tally(std::unordered_map<std::tuple<T...>, K> &m) {
+  // Map to Set. Can remove the last elements who is the `api name`
+  typedef decltype(make_tuple_without_last(std::declval<std::tuple<T...>>())) Minusone;
+  std::set<Minusone> s;
+  for (auto &[k, v] : m)
+    s.insert(make_tuple_without_last(k));
+  // Get the Tally
+  std::vector<size_t> v{};
+  _get_uniq_tally(s, v);
+  // Reverse so hostname is first
+  std::reverse(v.begin(), v.end());
+  return v;
 }
 
-template<typename... K, typename V>
-auto get_uniq_tally(std::unordered_map<std::tuple<K...>, V> &input) {
-  auto tuple_set = std::make_tuple(std::set<K>{}...);
-  constexpr auto s = std::make_index_sequence<sizeof...(K)>();
-
-  for (auto const &m : input)
-    add_to_set(tuple_set, m.first, s);
-  return tuple_set;
-}
-
+// Footer for table
 template <typename TC, typename = std::enable_if_t<std::is_base_of_v<TallyCoreBase, TC>>>
 void add_footer(std::vector<std::pair<thapi_function_name, TC>> &m) {
   TC tot{};
@@ -202,8 +214,7 @@ EXAMPLE:
           }
 
 */
-template <typename K, typename V> 
-auto sort_by_value(const std::unordered_map<K, V> &m) {
+template <typename K, typename V> auto sort_by_value(const std::unordered_map<K, V> &m) {
   std::vector<std::pair<K, V>> v;
   std::copy(m.begin(), m.end(), std::back_inserter<std::vector<std::pair<K, V>>>(v));
   std::sort(v.begin(), v.end(), [=](auto &a, auto &b) { return a.second > b.second; });
@@ -246,9 +257,8 @@ auto max_string_size(std::vector<std::pair<thapi_function_name, TC>> &m,
   long name_max = header_name.size();
 
   // Know at compile time
-  auto tallycore_max =
-      std::apply([](auto &&...e) { return std::array{(static_cast<long>(strlen(e)))...}; },
-                 header_tallycore);
+  auto tallycore_max = std::apply(
+      [](auto &&...e) { return std::array{(static_cast<long>(strlen(e)))...}; }, header_tallycore);
 
   for (auto &[name, tallycore] : m) {
     name_max = std::max(static_cast<long>(name.size()), name_max);
@@ -273,14 +283,14 @@ auto max_string_size(std::vector<std::pair<thapi_function_name, TC>> &m,
 
 // We use 3 function, because my template skill are poor...
 template <size_t SIZE>
-std::ostream &operator<<(std::ostream &os,
-                         const std::pair<std::array<const char *, SIZE>, std::array<long, SIZE>> &_tup) {
+std::ostream &
+operator<<(std::ostream &os,
+           const std::pair<std::array<const char *, SIZE>, std::array<long, SIZE>> &_tup) {
   auto &[c, column_width] = _tup;
   for (auto i = 0U; i < c.size(); i++) {
     os << std::setw(std::abs(column_width[i]));
     if (column_width[i] <= 0)
-      os << ""
-         << "   ";
+      os << "" << "   ";
     else
       os << c[i] << " | ";
   }
@@ -288,15 +298,15 @@ std::ostream &operator<<(std::ostream &os,
 }
 
 // Print the TallyCore
-template <size_t SIZE, typename TC, typename = std::enable_if_t<std::is_base_of_v<TallyCoreString, TC>>>
+template <size_t SIZE, typename TC,
+          typename = std::enable_if_t<std::is_base_of_v<TallyCoreString, TC>>>
 std::ostream &operator<<(std::ostream &os, std::pair<TC, std::array<long, SIZE>> &_tup) {
   auto &[c, column_width] = _tup;
   const auto v = c.to_string();
   for (auto i = 0U; i < v.size(); i++) {
     os << std::setw(std::abs(column_width[i]));
     if (column_width[i] <= 0)
-      os << ""
-         << "   ";
+      os << "" << "   ";
     else
       os << v[i] << " | ";
   }
@@ -309,17 +319,15 @@ std::ostream &operator<<(std::ostream &os, std::pair<std::string, long> &pair) {
 }
 
 // Print 2 Tuple correspond to the hostname, process, ... device, subdevice.
-template <class... T, class... T2, size_t... I>
-void print_tally_header(std::ostream &os, const std::tuple<T...> &s, const std::tuple<T2...> &h,
-                 std::index_sequence<I...>) {
-  ((std::get<I>(s).size() ? os << std::get<I>(s).size() << " " << std::get<I>(h) << " | "
-                          : os << ""),
-   ...);
+template <class T, class... T2, size_t... I>
+void print_tally_header(std::ostream &os, const std::vector<T> &s, const std::tuple<T2...> &h,
+                        std::index_sequence<I...>) {
+  (((I < s.size()) ? os << s[I] << " " << std::get<I>(h) << " | " : os << ""), ...);
 }
 
-template <class... T, class... T2>
-void print_tally_header(std::ostream &os, const std::string &header, const std::tuple<T...> &s,
-                 const std::tuple<T2...> &h) {
+template <class T, class... T2>
+void print_tally_header(std::ostream &os, const std::string &header, const std::vector<T> &s,
+                        const std::tuple<T2...> &h) {
   os << header << " | ";
   constexpr auto seq = std::make_index_sequence<sizeof...(T2)>();
   print_tally_header(os, s, h, seq);
@@ -463,23 +471,18 @@ nlohmann::json json_extented(std::unordered_map<K, TC> &m, std::tuple<T...> &&h)
   return j;
 }
 
-
 // Wrapper type that does reversal
-template <typename Range>
-class Reverser {
-    Range& r_;
-  public:
-    using iterator_type = std::reverse_iterator<decltype(std::begin(r_))>;
+template <typename Range> class Reverser {
+  Range &r_;
 
-    Reverser(Range& r) : r_(r) {}
+public:
+  using iterator_type = std::reverse_iterator<decltype(std::begin(r_))>;
 
-    iterator_type begin() { return iterator_type(std::end(r_)); }
-    iterator_type end()   { return iterator_type(std::begin(r_)); }
+  Reverser(Range &r) : r_(r) {}
+
+  iterator_type begin() { return iterator_type(std::end(r_)); }
+  iterator_type end() { return iterator_type(std::begin(r_)); }
 };
 
 // Helper creation function
-template <typename Range>
-Reverser<Range> reverse(Range& r)
-{
-    return Reverser<Range>(r);
-}
+template <typename Range> Reverser<Range> reverse(Range &r) { return Reverser<Range>(r); }
