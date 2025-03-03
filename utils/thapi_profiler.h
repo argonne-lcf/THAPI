a1b6fb189deee979fcc76720c9b855c4cdcb398b
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-03-03 22:46:54 +0000

    Add `profiler_` to ThapiProfiler symbols

--- utils/thapi_profiler.h
+++ utils/thapi_profiler.h
@@ -1,7 +1,7 @@
 #if !defined(THAPI_PROFILER)
 #define THAPI_PROFILER
 
-void thapi_stop();
-void thapi_start();
+void thapi_profiler_start();
+void thapi_profiler_stop();
 
 #endif // THAPI_PROFILER
