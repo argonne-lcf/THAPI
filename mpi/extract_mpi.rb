5bf24c09887bff7ce748cfd1812a6cd1dec4c05b
tapplencourt <tapplencourt@anl.gov>
2025-03-17 15:40:14 +0000

    fix mpi

--- mpi/extract_mpi.rb
+++ mpi/extract_mpi.rb
@@ -1,10 +1,24 @@
 require_relative 'extract_base'
 
-preprocessed_sources_omp_api = $cpp.preprocess(<<~EOF).gsub(/^#.*?$/, '')
+mpi_header = <<~EOF
   #include <mpi.h>
 EOF
 
-ast = $parser.parse(preprocessed_sources_omp_api)
+if enable_clang_parser?
+  header = [shared_header, mpi_header].join("\n")
+  require 'open3'
+  yaml, = Open3.capture2('h2yaml -xc -I modified_include/ -', stdin_data: header)
+else
+
+  preprocessed_sources_mpi_api = $cpp.preprocess(<<~EOF).gsub(/^#.*?$/, '')
+    #{mpi_header}
+  EOF
+
+  ast = $parser.parse(preprocessed_sources_mpi_api)
+  yaml = ast.extract_declarations.to_yaml
+
+end
+
 File.open('mpi_api.yaml', 'w') do |f|
-  f.puts ast.extract_declarations.to_yaml
+  f.puts yaml
 end
