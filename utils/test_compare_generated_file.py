6fc4771d320f9925ac5f4ed2449c7c97398bbc8b
tapplencourt <tapplencourt@anl.gov>
2025-03-20 20:27:01 +0000

    try check

--- utils/test_compare_generated_file.py
+++ utils/test_compare_generated_file.py
@@ -5,34 +5,31 @@ import pytest
 import yaml
 
 # Please put the corrects paths
-stems = [
-    "/home/applenco/tmp/THAPI_old_parser/build/",
-    "/home/applenco/tmp/THAPI_new_parser/build/",
-]
+stems = [ os.environ['THAPI_REF'], os.environ['THAPI_NEW'] ]
 
 filenames = []
-# filenames += ["mpi/tracer_mpi.c","mpi/mpi_library.rb"]
-filenames += ["mpi/mpi_api.yaml"]
+filenames += ["mpi/tracer_mpi.c","mpi/mpi_library.rb"]
+#filenames += ["mpi/mpi_api.yaml"]
 
-# filenames += ["omp/tracer_ompt.c","omp/omp_library.rb"]
-filenames += ["omp/ompt_api.yaml"]
+filenames += ["omp/tracer_ompt.c","omp/omp_library.rb"]
+#filenames += ["omp/ompt_api.yaml"]
 
-# filenames += ["hip/tracer_hip.c","hip/hip_library.rb"]
-filenames += ["hip/hip_api.yaml"]
+filenames += ["hip/tracer_hip.c","hip/hip_library.rb"]
+#filenames += ["hip/hip_api.yaml"]
 
-# filenames += ["cuda/tracer_cuda.c","cuda/cuda_library.rb"]
-filenames += ["cuda/cuda_api.yaml"]
+filenames += ["cuda/tracer_cuda.c","cuda/cuda_library.rb"]
+#filenames += ["cuda/cuda_api.yaml"]
 
-# filenames += ["cuda/tracer_cudart.c","cuda/cudart_library.rb"]
-filenames += ["cuda/cudart_api.yaml"]
+filenames += ["cuda/tracer_cudart.c","cuda/cudart_library.rb"]
+#filenames += ["cuda/cudart_api.yaml"]
 
-# filenames += ["ze/tracer_ze.c","ze/ze_library.rb"]
-filenames += [
-    "ze/ze_api.yaml",
-    "ze/zes_api.yaml",
-    "ze/zel_api.yaml",
-    "ze/zex_api.yaml",
-]
+filenames += ["ze/tracer_ze.c","ze/ze_library.rb"]
+#filenames += [
+#    "ze/ze_api.yaml",
+#    "ze/zes_api.yaml",
+#    "ze/zel_api.yaml",
+#    "ze/zex_api.yaml",
+#]
 
 
 def load_file(path):
