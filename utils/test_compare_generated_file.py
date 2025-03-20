f223a34afdf096b128b88be0d66a781f16e8dcb4
Thomas Applencourt <tapplencourt@anl.gov>
2025-03-20 16:38:05 -0500

    Compare generated file stability (#333)
    * try check
    
    * fix ci
    
    * less file
    
    * remove file who didn't exist
    
    * fix step
    
    * stupid...
    
    * fix script

--- utils/test_compare_generated_file.py
+++ utils/test_compare_generated_file.py
@@ -1,38 +1,34 @@
-import pathlib
-
+import os
 # We suggest to install `pytest-icdiff` to get better diff
 import pytest
 import yaml
 
 # Please put the corrects paths
-stems = [
-    "/home/applenco/tmp/THAPI_old_parser/build/",
-    "/home/applenco/tmp/THAPI_new_parser/build/",
-]
+stems = [ os.environ['THAPI_REF'], os.environ['THAPI_NEW'] ]
 
 filenames = []
-# filenames += ["mpi/tracer_mpi.c","mpi/mpi_library.rb"]
-filenames += ["mpi/mpi_api.yaml"]
+filenames += ["mpi/tracer_mpi.c","mpi/mpi_library.rb"]
+#filenames += ["mpi/mpi_api.yaml"]
 
-# filenames += ["omp/tracer_ompt.c","omp/omp_library.rb"]
-filenames += ["omp/ompt_api.yaml"]
+filenames += ["omp/tracer_ompt.c","omp/omp_library.rb"]
+#filenames += ["omp/ompt_api.yaml"]
 
-# filenames += ["hip/tracer_hip.c","hip/hip_library.rb"]
-filenames += ["hip/hip_api.yaml"]
+filenames += ["hip/tracer_hip.c","hip/hip_library.rb"]
+#filenames += ["hip/hip_api.yaml"]
 
-# filenames += ["cuda/tracer_cuda.c","cuda/cuda_library.rb"]
-filenames += ["cuda/cuda_api.yaml"]
+filenames += ["cuda/tracer_cuda.c","cuda/cuda_library.rb"]
+#filenames += ["cuda/cuda_api.yaml"]
 
-# filenames += ["cuda/tracer_cudart.c","cuda/cudart_library.rb"]
-filenames += ["cuda/cudart_api.yaml"]
+filenames += ["cuda/tracer_cudart.c"]
+#filenames += ["cuda/cudart_api.yaml"]
 
-# filenames += ["ze/tracer_ze.c","ze/ze_library.rb"]
-filenames += [
-    "ze/ze_api.yaml",
-    "ze/zes_api.yaml",
-    "ze/zel_api.yaml",
-    "ze/zex_api.yaml",
-]
+filenames += ["ze/tracer_ze.c","ze/ze_library.rb"]
+#filenames += [
+#    "ze/ze_api.yaml",
+#    "ze/zes_api.yaml",
+#    "ze/zel_api.yaml",
+#    "ze/zex_api.yaml",
+#]
 
 
 def load_file(path):
@@ -41,10 +37,8 @@ def load_file(path):
             return yaml.safe_load(f)
         return f.readlines()
 
-
-@pytest.mark.parametrize(
-    "path_ref,path_new", [(stems[0] + n, stems[1] + n) for n in filenames]
-)
+all_tuples = [ tuple( os.path.join(stem,n) for stem in stems ) for n in filenames ]
+@pytest.mark.parametrize("path_ref,path_new", all_tuples)
 def test_filter_include(path_ref, path_new):
     ref_ = load_file(path_ref)
     new_ = load_file(path_new)
