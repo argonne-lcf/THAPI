bfabb96b6c257b98cc59bd1929990be9a1cd4fb3
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-03-25 22:38:13 +0000

    Check if the header is included by a CXX compiler

--- utils/thapi.h
+++ utils/thapi.h
@@ -1,7 +1,15 @@
 #if !defined(THAPI)
 #define THAPI
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void thapi_start(void);
 void thapi_stop(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // THAPI
