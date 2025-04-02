96bdf34d0e38737e6bf1e9e7425f546a147c053a
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-04-02 06:10:06 +0000

    Format utils/thapi_toggle.c

--- utils/thapi_toggle.c
+++ utils/thapi_toggle.c
@@ -1,14 +1,13 @@
-#include "thapi_tracepoints.h"
 #include "thapi.h"
+#include "thapi_tracepoints.h"
 
 #ifndef LTTNG_UST_CONSTRUCTOR_PRIO
-#error "LTTNG_UST_CONSTRUCTOR_PRIO not defined."
+#error "LTTNG_UST_CONSTRUCTOR_PRIO is not defined."
 #endif
 
-void thapi_start(void) {
-  tracepoint(lttng_ust_toggle, start);
-}
+void thapi_start(void) { tracepoint(lttng_ust_toggle, start); }
 
-void __attribute__((constructor(LTTNG_UST_CONSTRUCTOR_PRIO + 1))) thapi_stop(void) {
+void __attribute__((constructor(LTTNG_UST_CONSTRUCTOR_PRIO + 1)))
+thapi_stop(void) {
   tracepoint(lttng_ust_toggle, stop);
 }
