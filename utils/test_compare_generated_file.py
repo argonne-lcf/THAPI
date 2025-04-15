777c94a414ca9292e55518bad8e1e5fb7b74e406
Thomas Applencourt <tapplencourt@anl.gov>
2025-04-15 23:26:16 +0200

    Cleaner test (#342)
    * cleaner
    
    * fix comment
    
    * Update utils/test_compare_generated_so.py
    
    Co-authored-by: Brice Videau <bvideau@anl.gov>
    
    * I Hate code duplication but ...
    
    * fix
    
    * fix opencl
    
    ---------
    
    Co-authored-by: Brice Videau <bvideau@anl.gov>

--- utils/test_compare_generated_file.py
+++ utils/test_compare_generated_file.py
@@ -1,34 +1,35 @@
 import os
+
 # We suggest to install `pytest-icdiff` to get better diff
 import pytest
 import yaml
 
 # Please put the corrects paths
-stems = [ os.environ['THAPI_REF'], os.environ['THAPI_NEW'] ]
+stems = [os.environ["THAPI_REF"], os.environ["THAPI_NEW"]]
 
 filenames = []
-filenames += ["mpi/tracer_mpi.c","mpi/mpi_library.rb"]
-#filenames += ["mpi/mpi_api.yaml"]
+filenames += ["mpi/tracer_mpi.c", "mpi/mpi_library.rb"]
+# filenames += ["mpi/mpi_api.yaml"]
 
-filenames += ["omp/tracer_ompt.c","omp/omp_library.rb"]
-#filenames += ["omp/ompt_api.yaml"]
+filenames += ["omp/tracer_ompt.c", "omp/omp_library.rb"]
+# filenames += ["omp/ompt_api.yaml"]
 
-filenames += ["hip/tracer_hip.c","hip/hip_library.rb"]
-#filenames += ["hip/hip_api.yaml"]
+filenames += ["hip/tracer_hip.c", "hip/hip_library.rb"]
+# filenames += ["hip/hip_api.yaml"]
 
-filenames += ["cuda/tracer_cuda.c","cuda/cuda_library.rb"]
-#filenames += ["cuda/cuda_api.yaml"]
+filenames += ["cuda/tracer_cuda.c", "cuda/cuda_library.rb"]
+# filenames += ["cuda/cuda_api.yaml"]
 
 filenames += ["cuda/tracer_cudart.c"]
-#filenames += ["cuda/cudart_api.yaml"]
+# filenames += ["cuda/cudart_api.yaml"]
 
-filenames += ["ze/tracer_ze.c","ze/ze_library.rb"]
-#filenames += [
+filenames += ["ze/tracer_ze.c", "ze/ze_library.rb"]
+# filenames += [
 #    "ze/ze_api.yaml",
 #    "ze/zes_api.yaml",
 #    "ze/zel_api.yaml",
 #    "ze/zex_api.yaml",
-#]
+# ]
 
 
 def load_file(path):
@@ -37,9 +38,12 @@ def load_file(path):
             return yaml.safe_load(f)
         return f.readlines()
 
-all_tuples = [ tuple( os.path.join(stem,n) for stem in stems ) for n in filenames ]
+
+all_tuples = [tuple(os.path.join(stem, n) for stem in stems) for n in filenames]
+
+
 @pytest.mark.parametrize("path_ref,path_new", all_tuples)
-def test_filter_include(path_ref, path_new):
+def test_code(path_ref, path_new):
     ref_ = load_file(path_ref)
     new_ = load_file(path_new)
     assert ref_ == new_
