740b3f84edd74a1a556042000d5e4d8a0d1842ca
Thomas Applencourt <tapplencourt@anl.gov>
2025-04-15 18:14:11 +0200

    Symbol check (#341)
    * add test_compare_generated_so.py
    
    * use libs
    
    * edit workflow

---
+++ utils/test_compare_generated_so.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+# We suggest to install `pytest-icdiff` to get better diff
+import pytest
+import yaml
+
+# Please put the corrects paths
+stems = [ os.environ['THAPI_REF'], os.environ['THAPI_NEW'] ]
+
+filenames = []
+filenames += ["ze/.libs/libze_loader.so"]
+filenames += ["opencl/.libs/libOpenCL.so"]
+filenames += ["hip/.libs/libamdhip64.so"]
+filenames += ["cuda/.libs/libcudart.so"]
+filenames += ["cuda/.libs/libcuda.so"]
+filenames += ["mpi/.libs/libmpi.so"]
+
+
+def load_so(path):
+    symbols_ = subprocess.run(['nm', '-D', path], capture_output=True, text=True, check=True).stdout
+    symbols = map(str.split, symbols_.splitlines())
+    return sorted(line[2] for line in symbols if line[1] == 'T')
+
+all_tuples = [ tuple( os.path.join(stem,n) for stem in stems ) for n in filenames ]
+@pytest.mark.parametrize("path_ref,path_new", all_tuples)
+def test_filter_include(path_ref, path_new):
+    ref_ = load_so(path_ref)
+    new_ = load_so(path_new)
+    assert ref_ == new_
