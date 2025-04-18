fdf3589f7fa30a5c45e52b5c9e9be60f8536ed43
tapplencourt <tapplencourt@anl.gov>
2025-04-18 21:21:58 +0000

    remove deadcode

--- omp/btx_ompinterval_callbacks.cpp
+++ omp/btx_ompinterval_callbacks.cpp
@@ -44,37 +44,15 @@ static void host_op_callback(void *btx_handle, void *usr_data, int64_t ts, const
                                 op_name.c_str(), (ts - start_ts.value()), err);
   }
 }
-/*
-static void host_and_traffic_op_callback(void *btx_handle, void *usr_data, int64_t ts,
+
+static void traffic_op_callback(void *btx_handle, void *usr_data, int64_t ts,
                                          const char *hostname, int64_t vpid, uint64_t vtid,
                                          ompt_scope_endpoint_t endpoint, size_t bytes,
                                          std::string const &op_name) {
   if (auto start_ts = set_or_get_start(usr_data, {hostname, vpid, vtid, op_name}, endpoint, ts)) {
-    const bool err = false;
-    btx_push_message_lttng_host(btx_handle, hostname, vpid, vtid, start_ts.value(), BACKEND_OMP,
-                                op_name.c_str(), (ts - start_ts.value()), err);
     btx_push_message_lttng_traffic(btx_handle, hostname, vpid, vtid, start_ts.value(), BACKEND_OMP,
                                    op_name.c_str(), bytes, "");
   }
-}
-
-static void ompt_callback_target_callback(void *btx_handle, void *usr_data, int64_t ts,
-                                          const char *hostname, int64_t vpid, uint64_t vtid,
-                                          ompt_target_t kind, ompt_scope_endpoint_t endpoint,
-                                          int device_num, ompt_data_t *task_data,
-                                          ompt_id_t target_id, void *codeptr_ra) {
-  std::string op_name(magic_enum::enum_name(kind));
-  host_op_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, endpoint, op_name);
-}
-
-static void ompt_callback_target_emi_callback(void *btx_handle, void *usr_data, int64_t ts,
-                                              const char *hostname, int64_t vpid, uint64_t vtid,
-                                              ompt_target_t kind, ompt_scope_endpoint_t endpoint,
-                                              int device_num, ompt_data_t *task_data,
-                                              ompt_data_t *target_task_data,
-                                              ompt_data_t *target_data, void *codeptr_ra) {
-  std::string op_name(magic_enum::enum_name(kind));
-  host_op_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, endpoint, op_name);
 }
 
 static void ompt_callback_target_data_op_callback(
@@ -82,7 +60,7 @@ static void ompt_callback_target_data_op_callback(
     ompt_id_t target_id, ompt_id_t host_op_id, ompt_target_data_op_t optype, void *src_addr,
     int src_device_num, void *dest_addr, int dest_device_num, size_t bytes, void *codeptr_ra) {
   std::string op_name(magic_enum::enum_name(optype));
-  host_and_traffic_op_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, ompt_scope_beginend,
+  traffic_op_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, ompt_scope_beginend,
                                bytes, op_name);
 }
 
@@ -92,39 +70,10 @@ static void ompt_callback_target_data_op_emi_callback(
     ompt_id_t *host_op_id, ompt_target_data_op_t optype, void *src_addr, int src_device_num,
     void *dest_addr, int dest_device_num, size_t bytes, void *codeptr_ra) {
   std::string op_name(magic_enum::enum_name(optype));
-  host_and_traffic_op_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, endpoint, bytes,
+  traffic_op_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, endpoint, bytes,
                                op_name);
 }
 
-static void ompt_callback_target_submit_callback(void *btx_handle, void *usr_data, int64_t ts,
-                                                 const char *hostname, int64_t vpid, uint64_t vtid,
-                                                 ompt_id_t target_id, ompt_id_t host_op_id,
-                                                 unsigned int requested_num_teams) {
-  // Nothing we can do with them, but add a callback to they are removed from the trace
-}
-
-static void ompt_callback_target_submit_emi_callback(void *btx_handle, void *usr_data, int64_t ts,
-                                                     const char *hostname, int64_t vpid,
-                                                     uint64_t vtid, ompt_scope_endpoint_t endpoint,
-                                                     ompt_data_t *target_data,
-                                                     ompt_id_t *host_op_id,
-                                                     unsigned int requested_num_teams) {
-  host_op_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, endpoint,
-                   "ompt_target_submit_emi");
-}
-
-static void ompt_callback_sync_region_callback(void *btx_handle, void *usr_data, int64_t ts,
-                                                     const char *hostname, int64_t vpid,
-                                                     uint64_t vtid, ompt_sync_region_t kind,
-                                            ompt_scope_endpoint_t endpoint,
-                                            ompt_data_t *parallel_data,
-                                            ompt_data_t *task_data,
-                                            void *codeptr_ra) {
-
-  std::string op_name(magic_enum::enum_name(kind));
-  host_op_callback(btx_handle, usr_data, ts, hostname, vpid, vtid, endpoint, op_name);
-}
-*/
 static void btx_host_sync_callback(void *btx_handle, void *usr_data, int64_t ts,
 						      const char* name,
                                                      const char *hostname, int64_t vpid,
@@ -162,15 +111,10 @@ static void btx_host_callback(void *btx_handle, void *usr_data, int64_t ts,
 void btx_register_usr_callbacks(void *btx_handle) {
   btx_register_callbacks_initialize_component(btx_handle, &btx_initialize_component);
   btx_register_callbacks_finalize_component(btx_handle, &btx_finalize_component);
-/*
-  REGISTER_ASSOCIATED_CALLBACK(ompt_callback_target);
-  REGISTER_ASSOCIATED_CALLBACK(ompt_callback_target_emi);
+
   REGISTER_ASSOCIATED_CALLBACK(ompt_callback_target_data_op);
   REGISTER_ASSOCIATED_CALLBACK(ompt_callback_target_data_op_emi);
-  REGISTER_ASSOCIATED_CALLBACK(ompt_callback_target_submit);
-  REGISTER_ASSOCIATED_CALLBACK(ompt_callback_target_submit_emi);
-  REGISTER_ASSOCIATED_CALLBACK(ompt_callback_sync_region);
-*/
+  
   btx_register_callbacks_btx_host_sync(btx_handle, &btx_host_sync_callback);
   btx_register_callbacks_btx_host_target(btx_handle, &btx_host_target_callback);
   btx_register_callbacks_btx_host(btx_handle, &btx_host_callback);
