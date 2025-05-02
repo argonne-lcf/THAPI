3f04eebfcde894e21d69e39a5fd3a3238c5d1159
Thomas Applencourt <tapplencourt@anl.gov>
2025-05-02 17:53:31 -0500

    Add model to tests compare (#352)

--- utils/test_compare_generated_file.py
+++ utils/test_compare_generated_file.py
@@ -8,30 +8,27 @@ import yaml
 stems = [os.environ["THAPI_REF"], os.environ["THAPI_NEW"]]
 
 filenames = []
-filenames += ["mpi/tracer_mpi.c", "mpi/mpi_library.rb"]
+filenames += ["mpi/tracer_mpi.c", "mpi/mpi_library.rb", "mpi/mpi_babeltrace_model.yaml"]
 # filenames += ["mpi/mpi_api.yaml"]
-filenames += ["mpi/mpi_tracepoints.tp"]
 
-filenames += ["omp/tracer_ompt.c", "omp/omp_library.rb"]
+filenames += ["omp/tracer_ompt.c", "omp/omp_library.rb", "omp/omp_babeltrace_model.yaml"]
 # filenames += ["omp/ompt_api.yaml"]
 
-filenames += ["hip/tracer_hip.c", "hip/hip_library.rb"]
+filenames += ["hip/tracer_hip.c", "hip/hip_library.rb", "hip/hip_babeltrace_model.yaml"]
 # filenames += ["hip/hip_api.yaml"]
 
-filenames += ["cuda/tracer_cuda.c", "cuda/cuda_library.rb"]
+filenames += ["cuda/tracer_cuda.c", "cuda/cuda_library.rb", "cuda/cuda_babeltrace_model.yaml"]
 # filenames += ["cuda/cuda_api.yaml"]
 
-filenames += ["cuda/tracer_cudart.c"]
-# filenames += ["cuda/cudart_api.yaml"]
+filenames += ["cuda/tracer_cudart.c", "cuda/cudart_api.yaml"]
 
-filenames += ["ze/tracer_ze.c", "ze/ze_library.rb"]
+filenames += ["ze/tracer_ze.c", "ze/ze_library.rb", "ze/ze_babeltrace_model.yaml"]
 # filenames += [
 #    "ze/ze_api.yaml",
 #    "ze/zes_api.yaml",
 #    "ze/zel_api.yaml",
 #    "ze/zex_api.yaml",
 # ]
-
 
 def load_file(path):
     with open(path, "r") as f:
