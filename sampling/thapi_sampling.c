b76e66c5d28edb12fe7e4a1c6c8987714026f123
tapplencourt <tapplencourt@anl.gov>
2025-06-30 21:54:07 +0000

    first pass

--- sampling/thapi_sampling.c
+++ sampling/thapi_sampling.c
@@ -7,13 +7,20 @@
 #include "thapi_sampling.h"
 #include "sampling.h"
 #include "utarray.h"
+#include <signal.h>
+#include <time.h>
+
+#define RT_SIGNAL_READY (SIGRTMIN)
+#define RT_SIGNAL_FINISH (SIGRTMIN + 3)
 
 struct sampling_entry {
-  void (*pfn)(void);
+  void (*pfn_run)(void);
   struct timespec interval;
+  void (*pfn_final)(void);
   struct timespec next;
 };
 
+typedef void (*plugin_init_func)();
 
 static pthread_mutex_t thapi_sampling_mutex = PTHREAD_MUTEX_INITIALIZER;
 static UT_array *thapi_sampling_events = NULL;
@@ -21,18 +28,26 @@ static UT_array *thapi_sampling_events = NULL;
 static pthread_once_t thapi_init_once = PTHREAD_ONCE_INIT;
 static volatile int thapi_sampling_finished = 0;
 static volatile int thapi_sampling_initialized = 0;
-static pthread_t thapi_sampling_thread;
+
+static void signal_handler_finish(int signum) {
+  printf("signal_handler_finish\n");
+  if (signum == RT_SIGNAL_FINISH) {
+    thapi_sampling_finished = 1;
+  }
+}
 
 static void __attribute__((destructor))
 thapi_sampling_cleanup() {
   if (!thapi_sampling_initialized)
     return;
   thapi_sampling_finished = 1;
-  pthread_join(thapi_sampling_thread, NULL);
   pthread_mutex_lock(&thapi_sampling_mutex);
   struct sampling_entry **entry = NULL;
-  while ((entry = (struct sampling_entry **)utarray_next(thapi_sampling_events, entry)))
+  while ((entry = (struct sampling_entry **)utarray_next(thapi_sampling_events, entry))) {
+    if ( (*entry)->pfn_final )
+        (*entry)->pfn_final();
     free(*entry);
+  }
   utarray_free(thapi_sampling_events);
   pthread_mutex_unlock(&thapi_sampling_mutex);
 }
@@ -66,7 +81,7 @@ static inline void time_add(struct timespec *dest, const struct timespec *t, con
   }
 }
 
-void * thapi_sampling_loop(void *args) {
+static void * thapi_sampling_loop(void *args) {
   (void)args;
   while(!thapi_sampling_finished) {
     struct timespec now;
@@ -76,7 +91,7 @@ void * thapi_sampling_loop(void *args) {
     clock_gettime(CLOCK_REALTIME, &now);
     while ((entry = (struct sampling_entry **)utarray_next(thapi_sampling_events, entry)) &&
            time_cmp(&(*entry)->next, &now) < 0) {
-      (*entry)->pfn();
+      (*entry)->pfn_run();
       time_add(&(*entry)->next, &(*entry)->next, &(*entry)->interval);
       if(time_cmp(&(*entry)->next, &now) < 0)
 	      time_add(&(*entry)->next, &now, &(*entry)->interval);
@@ -92,43 +107,39 @@ void * thapi_sampling_loop(void *args) {
 }
 
 static void thapi_sampling_heartbeat() {
+  printf("thapi_sampling\n");
   do_tracepoint(lttng_ust_sampling, heartbeat, 16);
 }
 
+static void thapi_sampling_heartbeat_finalize() {
+   printf("thapi_sampling_finalize\n");
+   do_tracepoint(lttng_ust_sampling, heartbeat, 32);
+}
+
 static void thapi_sampling_heartbeat2() {
+  printf("thapi_sampling_heartbeat2\n");
   do_tracepoint(lttng_ust_sampling, heartbeat2);
 }
 
-void thapi_sampling_init_once() {
-  struct timespec interval;
+
+static void thapi_sampling_init_once() {
   utarray_new(thapi_sampling_events, &ut_ptr_icd);
   if (!thapi_sampling_events)
     return;
-  if (getenv("LTTNG_UST_SAMPLING_HEARTBEAT")) {
-    interval.tv_sec = 0;
-    interval.tv_nsec = 100000000;
-    thapi_register_sampling(&thapi_sampling_heartbeat, &interval);
-  }
-  if (getenv("LTTNG_UST_SAMPLING_HEARTBEAT2")) {
-    interval.tv_sec = 0;
-    interval.tv_nsec = 30000000;
-    thapi_register_sampling(&thapi_sampling_heartbeat2, &interval);
-  }
-  if (!pthread_create(&thapi_sampling_thread, NULL, &thapi_sampling_loop, NULL))
-    thapi_sampling_initialized = 1;
+  thapi_sampling_initialized = 1;
 }
 
-int thapi_sampling_init() {
-  if (getenv("LTTNG_UST_SAMPLING"))
-    pthread_once(&thapi_init_once, &thapi_sampling_init_once);
-  return 1;
+static int thapi_sampling_init() {
+  pthread_once(&thapi_init_once, &thapi_sampling_init_once);
+  return 0;
 }
 
-thapi_sampling_handle_t thapi_register_sampling(void (*pfn)(void), struct timespec *interval) {
+void thapi_register_sampling(void (*pfn_run)(void), struct timespec *interval,
+					        void (*pfn_final)(void) ) {
   struct sampling_entry *entry = NULL;
   struct timespec now, next;
   if(clock_gettime(CLOCK_REALTIME, &now))
-    return NULL;
+    return;
   time_add(&next, &now, interval);
 
   pthread_mutex_lock(&thapi_sampling_mutex);
@@ -137,30 +148,60 @@ thapi_sampling_handle_t thapi_register_sampling(void (*pfn)(void), struct timesp
   entry = (struct sampling_entry *)malloc(sizeof(struct sampling_entry));
   if (!entry)
     goto end;
-  entry->pfn = pfn;
+  entry->pfn_run = pfn_run;
   entry->interval = *interval;
+  entry->pfn_final = pfn_final;
+
   entry->next = next;
   utarray_push_back(thapi_sampling_events, &entry);
   utarray_sort(thapi_sampling_events, sampling_entry_cmpw);
 end:
   pthread_mutex_unlock(&thapi_sampling_mutex);
-  return entry;
+  return;
 }
 
-void thapi_unregister_sampling(thapi_sampling_handle_t handle)
-{
-  if (!handle)
-    return;
-  struct sampling_entry *entry = (struct sampling_entry *)handle;
-  pthread_mutex_lock(&thapi_sampling_mutex);
-  unsigned int len = utarray_len(thapi_sampling_events);
-  for (unsigned int i = 0; i < len; i++) {
-    struct sampling_entry **p =
-      (struct sampling_entry **)utarray_eltptr(thapi_sampling_events, i);
-    if (*p == entry) {
-      utarray_erase(thapi_sampling_events, i, 1);
-      break;
-    }
+int main(int argc, char **argv) {
+
+  int parent_pid = 0;
+  parent_pid = atoi(argv[1]);
+   
+  // Setup signaling, to exist the sampling loop
+  sigset_t signal_set;
+  sigemptyset(&signal_set);
+  sigaddset(&signal_set, RT_SIGNAL_FINISH);
+  signal(RT_SIGNAL_FINISH, signal_handler_finish);
+
+  // Initlixaztion
+  thapi_sampling_init();
+
+  // Register test sample    
+  if (getenv("LTTNG_UST_SAMPLING_HEARTBEAT")) {
+    struct timespec interval;
+    interval.tv_sec = 1;
+    interval.tv_nsec = 100000000;
+    thapi_register_sampling(&thapi_sampling_heartbeat, &interval, &thapi_sampling_heartbeat_finalize);
   }
-  pthread_mutex_unlock(&thapi_sampling_mutex);
+  if (getenv("LTTNG_UST_SAMPLING_HEARTBEAT2")) {
+    struct timespec interval;
+    interval.tv_sec = 2;
+    interval.tv_nsec = 30000000;
+    thapi_register_sampling(&thapi_sampling_heartbeat2, &interval, NULL);
+  }
+
+  // Regiser user sample
+  for (int i=2 ; i < argc; i++) {
+     void *handle = dlopen(argv[i], RTLD_LAZY | RTLD_LOCAL | RTLD_DEEPBIND);
+     plugin_init_func f = (plugin_init_func)(intptr_t)dlsym(handle, "thapi_register_sampling_plugin");
+     f();
+  }
+
+  // Signal Ready to manager
+  kill(parent_pid, RT_SIGNAL_READY);
+
+  // Run until signal is comming
+  thapi_sampling_loop(NULL);
+  // Call destructor
+  kill(parent_pid, RT_SIGNAL_READY);
+
+    return 0;
 }
--- sampling/thapi_sampling.h
+++ sampling/thapi_sampling.h
@@ -1,14 +1,5 @@
-#include <time.h>
-
-typedef void * thapi_sampling_handle_t;
-
-extern int thapi_sampling_init();
-
-extern thapi_sampling_handle_t
-thapi_register_sampling(
-	void (*pfn)(void),
-	struct timespec *interval);
-
 extern void
-thapi_unregister_sampling(
-	thapi_sampling_handle_t handle);
+thapi_register_sampling(
+	void (*pfn_run)(void) /*Running*/,
+	struct timespec *interval,
+	void (*pfn_final)(void) /*Finalization*/);
