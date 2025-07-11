ce38617fc86dc76599e7e3b455d99dbbc3b712dc
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-07-11 16:13:09 +0000

    Get rid of THAPI_INSTALL_DIR

--- integration_tests/test_env_vars.bash
+++ integration_tests/test_env_vars.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 export THAPI_HOME=${THAPI_HOME:-${PWD}}
-export THAPI_INSTALL_DIR=${THAPI_INSTALL_DIR:-${PWD}/build/ici}
-export THAPI_BIN_DIR=${THAPI_BIN_DIR:-${THAPI_INSTALL_DIR}/bin}
+export THAPI_BIN_DIR=${THAPI_BIN_DIR:-${PWD}/build/ici/bin}
 export THAPI_TEST_BIN=${THAPI_TEST_BIN:-clinfo}
 
 export IPROF=${IPROF:-${THAPI_BIN_DIR}/iprof}
