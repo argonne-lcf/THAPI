bb9ddc81ec1709ea589e5701e08927c0f07ec9e8
tapplencourt <tapplencourt@anl.gov>
2025-09-08 17:10:07 +0000

    rubocop

--- utils/gen_custom_probes.rb
+++ utils/gen_custom_probes.rb
@@ -5,23 +5,23 @@ events_path = ARGV[0]
 namespace = ARGV[1]
 header = ARGV[2]
 
-raise "No events to load!" unless events_path
-raise "No namespace provided!" unless namespace
+raise 'No events to load!' unless events_path
+raise 'No namespace provided!' unless namespace
 
-h = YAML::load_file(events_path)[namespace]
+h = YAML.load_file(events_path)[namespace]
 
 raise "Invalid namespace: #{namespace}!" unless h
 
-["lttng/tracepoint_gen.h", header].filter.each { |h|
-  puts %Q[#include "#{h}"]
-}
+['lttng/tracepoint_gen.h', header].filter.each do |h|
+  puts %(#include "#{h}")
+end
 
 puts
 
-h["enums"].to_a.each { |e|
-    LTTng.print_enum(namespace, e)
-}
+h['enums'].to_a.each do |e|
+  LTTng.print_enum(namespace, e)
+end
 
-h["events"].to_a.each { |e|
+h['events'].to_a.each do |e|
   LTTng.print_tracepoint(namespace, e)
-}
+end
