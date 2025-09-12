c619985a410397a9d37c3e17e974f380adc8b2be
tapplencourt <tapplencourt@anl.gov>
2025-09-12 22:19:09 +0000

    progress?

---
+++ backends/itt/gen_itt.rb
@@ -0,0 +1,45 @@
+require_relative 'itt_model'
+
+puts <<EOF
+#include "ittnotify.h"
+EOF
+
+
+common_block = lambda { |c, provider|
+  params = c.parameters.collect(&:name)
+  tp_params = c.parameters.collect { |p|
+    if p.type.kind_of?(YAMLCAst::Pointer) && p.type.type.kind_of?(YAMLCAst::Function)
+      "(void *)(intptr_t)" + p.name
+    else
+      p.name
+    end
+  }
+  tracepoint_params = c.tracepoint_parameters.collect(&:name)
+  c.tracepoint_parameters.each { |p|
+    puts "  #{p.type} #{p.name};"
+  }
+  c.tracepoint_parameters.each { |p|
+    puts p.init unless p.after?
+  }
+  tracepoint_params = c.tracepoint_parameters.reject { |p| p.after? }.collect(&:name)
+  puts <<EOF
+  tracepoint(#{provider}, #{c.name.gsub(/_func\z/,'')}, #{(tp_params+tracepoint_params).join(", ")});
+EOF
+}
+
+normal_wrapper = lambda { |c, provider|
+  puts <<EOF
+static #{c.decl} {
+EOF
+  common_block.call(c, provider)
+  puts <<EOF
+}
+
+EOF
+}
+
+$itt_commands.each { |c|
+  normal_wrapper.call(c, :lttng_ust_itt)
+}
+
+#puts File::read("tracer_ompt_helpers.include.c")
---
+++ backends/itt/gen_itt_tracepoints.rb
@@ -0,0 +1,14 @@
+require_relative 'itt_model'
+require_relative '../../utils/gen_probe_base'
+
+provider = :lttng_ust_itt
+
+puts <<EOF
+#include "lttng/tracepoint_gen.h"
+#include "ittnotify.h"
+EOF
+
+$itt_commands.each { |c|
+  next if c.parameters && c.parameters.length > LTTNG_USABLE_PARAMS
+  $tracepoint_lambda.call(provider, c)
+}
