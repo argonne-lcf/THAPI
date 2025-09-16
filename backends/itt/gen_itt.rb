5bd6c2e1ffcc45a5c597d178b2dd503bbf75348b
Thomas Applencourt <tapplencourt@anl.gov>
2025-09-16 19:29:33 +0000

    refactoring

--- backends/itt/gen_itt.rb
+++ backends/itt/gen_itt.rb
@@ -1,56 +1,19 @@
 require_relative 'itt_model'
 
-# Common Printing Function
-common_block = lambda { |c, provider|
-  params = c.parameters.collect(&:name)
-  
-  tracepoint_params = c.tracepoint_parameters.collect(&:name)
-  c.tracepoint_parameters.each { |p|
-    puts "  #{p.type} #{p.name};"
-  }
-  c.tracepoint_parameters.each { |p|
-    puts p.init unless p.after?
-  }
+# Customization of codegen
 
-  c.prologues.each { |e|
-    puts e
-  }
-
-  tracepoint_params = c.tracepoint_parameters.reject { |p| p.after? }.collect(&:name)
-  tracepoint_params.push("_retval") if c.has_return_type?
-
-  puts <<EOF
-  tracepoint(#{provider}, #{c.name}, #{(params+tracepoint_params).join(", ")});
-EOF
-
-  c.epilogues.each { |e|
-    puts e
-  }
-
-}
-
-# Customize Model for struct
-
-# Todo everything should done in one place
 $itt_commands.each { |c|
   next unless c.has_return_type?
 
-  register_prologue(c.name, 
-if c.type.kind_of?(YAMLCAst::Pointer)
-<<EOF
-  #{c.type} _retval = calloc(1, sizeof(*_retval));
-EOF
-else
-<<EOF
-  #{c.type} _retval = {};
-EOF
+  register_prologue(c.name,
+    if c.type.kind_of?(YAMLCAst::Pointer)
+      "#{c.type} _retval = calloc(1, sizeof(*_retval));"
+    else
+      "#{c.type} _retval = {};"
 end
 )
 
-  register_epilogue(c.name, <<EOF
-  return _retval;
-EOF
-)
+  register_epilogue(c.name, "return _retval;")
 }
 
 # Handshake, registering function pointer
@@ -95,15 +58,22 @@ EOF
 
 # Printing
 
-normal_wrapper = lambda { |c, provider|
-  puts <<EOF
-#{c.decl} {
-EOF
-  common_block.call(c, provider)
-  puts <<EOF
-}
+common_block = lambda { |c, provider|
 
-EOF
+  l = []
+
+  l += c.tracepoint_parameters.map { |p| "  #{p.type} #{p.name};"}
+  l += c.tracepoint_parameters.filter_map { |p| p.init unless p.after? }
+  l += c.prologues
+
+  params = c.parameters.collect(&:name)
+  tracepoint_params = c.tracepoint_parameters.collect(&:name)
+  tracepoint_params.push("_retval") if c.has_return_type?
+
+  l += ["tracepoint(#{provider}, #{c.name}, #{(params+tracepoint_params).join(", ")});"]
+  l += c.epilogues
+
+  "  "+l.join("\n  ")
 }
 
 puts <<EOF
@@ -112,6 +82,9 @@ puts <<EOF
 #include <stdlib.h>
 EOF
 
-$itt_commands.each { |c|
-  normal_wrapper.call(c, :lttng_ust_itt)
-}
+provider = :lttng_ust_itt
+puts $itt_commands.map { |c|
+  l  = ["#{c.decl} {"]
+  l += [common_block.call(c, provider)]
+  l += ["}"]
+}.join("  \n")
