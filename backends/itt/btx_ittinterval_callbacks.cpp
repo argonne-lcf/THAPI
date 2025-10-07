a3d3a2c48dade559351d2b708ab8cff1e2392049
tapplencourt <tapplencourt@anl.gov>
2025-10-07 16:41:37 +0000

    working?

--- backends/itt/btx_ittinterval_callbacks.cpp
+++ backends/itt/btx_ittinterval_callbacks.cpp
@@ -1,8 +1,13 @@
+#define INTEL_NO_MACRO_BODY
+#define INTEL_ITTNOTIFY_API_PRIVATE
 #include "ittnotify.h"
+#include "ittnotify_config.h"
+
 #include "xprof_utils.hpp"
 #include <algorithm>
 #include <cstring>
 #include <iomanip>
+#include <iostream>
 #include <metababel/metababel.h>
 #include <sstream>
 #include <stack>
@@ -58,8 +63,8 @@ template <typename T> static std::string extract_and_format(size_t count, void *
   return oss.str();
 }
 
-static std::string format_numeric_meta(__itt_metadata_type type, size_t count,
-                                       void *data_val_bytes) {
+static std::string
+format_numeric_meta(__itt_metadata_type type, size_t count, void *data_val_bytes) {
   switch (type) {
   case __itt_metadata_u64:
     return extract_and_format<uint64_t>(count, data_val_bytes);
@@ -83,9 +88,14 @@ static std::string format_numeric_meta(__itt_metadata_type type, size_t count,
 }
 
 // Push instantaneous "metadata: key=value" message on the stream
-static inline void push_meta_message(void *btx_handle, const char *hostname, int64_t vpid,
-                                     uint64_t vtid, int64_t ts, const std::string &scope_tag,
-                                     const std::string &key, const std::string &value) {
+static inline void push_meta_message(void *btx_handle,
+                                     const char *hostname,
+                                     int64_t vpid,
+                                     uint64_t vtid,
+                                     int64_t ts,
+                                     const std::string &scope_tag,
+                                     const std::string &key,
+                                     const std::string &value) {
   const bool err = false;
   std::string op = "meta[" + scope_tag + "]: " + key + "=" + value;
   btx_push_message_lttng_host(btx_handle, hostname, vpid, vtid, ts, BACKEND_ITT, op.c_str(), 1,
@@ -93,9 +103,13 @@ static inline void push_meta_message(void *btx_handle, const char *hostname, int
 }
 
 // Attach to current task's metadata vector
-static inline void attach_to_current_task_meta(data_t *state, const char *hostname, int64_t vpid,
-                                               uint64_t vtid, __itt_domain *domain,
-                                               const std::string &key, const std::string &value) {
+static inline void attach_to_current_task_meta(data_t *state,
+                                               const char *hostname,
+                                               int64_t vpid,
+                                               uint64_t vtid,
+                                               __itt_domain *domain,
+                                               const std::string &key,
+                                               const std::string &value) {
   auto k = hpt_domain_handle_t{hostname, vpid, vtid, domain};
   auto it = state->domain_handle_task_meta_stack.find(k);
   if (it == state->domain_handle_task_meta_stack.end() || it->second.empty()) {
@@ -115,35 +129,39 @@ static inline void attach_to_current_task_meta(data_t *state, const char *hostna
 //   /   _. | | |_   _.  _ |   _
 //   \_ (_| | | |_) (_| (_ |< _>
 //
-static void lttng_ust_itt___itt_string_handle_create_callback(void *btx_handle, void *usr_data,
-                                                              int64_t ts, const char *hostname,
-                                                              int64_t vpid, uint64_t vtid,
-                                                              __itt_string_handle *ittResult,
-                                                              char *str, char *str_val) {
 
-  auto *state = static_cast<data_t *>(usr_data);
-  state->itt_string_handle2name[{hostname, vpid, ittResult}] = std::string{str_val};
-}
+// Tasks
 
-static void lttng_ust_itt___itt_task_begin_callback(void *btx_handle, void *usr_data, int64_t ts,
-                                                    const char *hostname, int64_t vpid,
-                                                    uint64_t vtid, __itt_domain *domain, __itt_id,
-                                                    __itt_id, __itt_string_handle *name) {
+static void lttng_ust_itt___itt_task_begin_callback(void *btx_handle,
+                                                    void *usr_data,
+                                                    int64_t ts,
+                                                    const char *hostname,
+                                                    int64_t vpid,
+                                                    uint64_t vtid,
+                                                    __itt_domain *domain,
+                                                    __itt_id /*taskid*/,
+                                                    __itt_id /*parentid*/,
+                                                    __itt_string_handle *name,
+                                                    char *domain__nameA_val,
+                                                    char *name__strA_val) {
   auto *state = static_cast<data_t *>(usr_data);
-  const auto str_name = state->itt_string_handle2name[{hostname, vpid, name}];
+  const auto str_name = std::string{domain__nameA_val} + ":" + std::string{name__strA_val};
   auto key = hpt_domain_handle_t{hostname, vpid, vtid, domain};
   state->domain_handle_task_meta_stack[key].push({ts, str_name, std::vector<std::string>{}});
 }
 
-static void lttng_ust_itt___itt_task_end_callback(void *btx_handle, void *usr_data, int64_t ts,
-                                                  const char *hostname, int64_t vpid, uint64_t vtid,
+static void lttng_ust_itt___itt_task_end_callback(void *btx_handle,
+                                                  void *usr_data,
+                                                  int64_t ts,
+                                                  const char *hostname,
+                                                  int64_t vpid,
+                                                  uint64_t vtid,
                                                   __itt_domain *domain) {
   auto *state = static_cast<data_t *>(usr_data);
   const auto key = hpt_domain_handle_t{hostname, vpid, vtid, domain};
 
   const auto &[start_ts, op_name, meta_vec] = state->domain_handle_task_meta_stack[key].top();
 
-  // FIXME need to add domain name to decorated name
   std::string decorated_name = op_name;
   if (!meta_vec.empty()) {
     decorated_name += " {" + join_csv(meta_vec) + "}";
@@ -156,10 +174,17 @@ static void lttng_ust_itt___itt_task_end_callback(void *btx_handle, void *usr_da
   state->domain_handle_task_meta_stack[key].pop();
 }
 
-static void lttng_ust_itt___itt_event_create_callback(void *btx_handle, void *usr_data,
-                                                      int64_t /*ts*/, const char *hostname,
-                                                      int64_t vpid, uint64_t /*vtid*/,
-                                                      __itt_event event, char *name, int namelen,
+// Event
+
+static void lttng_ust_itt___itt_event_create_callback(void *btx_handle,
+                                                      void *usr_data,
+                                                      int64_t /*ts*/,
+                                                      const char *hostname,
+                                                      int64_t vpid,
+                                                      uint64_t /*vtid*/,
+                                                      __itt_event event,
+                                                      char *name,
+                                                      int namelen,
                                                       char *name_val) {
   auto *state = static_cast<data_t *>(usr_data);
 
@@ -170,9 +195,13 @@ static void lttng_ust_itt___itt_event_create_callback(void *btx_handle, void *us
   state->event_id2name[{hostname, vpid, event}] = std::move(event_name);
 }
 
-static void lttng_ust_itt___itt_event_start_callback(void *btx_handle, void *usr_data, int64_t ts,
-                                                     const char *hostname, int64_t vpid,
-                                                     uint64_t vtid, int __itt_err,
+static void lttng_ust_itt___itt_event_start_callback(void *btx_handle,
+                                                     void *usr_data,
+                                                     int64_t ts,
+                                                     const char *hostname,
+                                                     int64_t vpid,
+                                                     uint64_t vtid,
+                                                     int __itt_err,
                                                      __itt_event event) {
   auto *state = static_cast<data_t *>(usr_data);
   state->event_id_stack[{hostname, vpid, vtid, event}].push(ts);
@@ -189,9 +218,13 @@ static void lttng_ust_itt___itt_event_start_callback(void *btx_handle, void *usr
 // You can overlap different ITT API events. In the case of overlapping events, the time is
 // considered to have been spent only in the event region with the later __itt_event_start().
 // Unmatched __itt_event_end() calls are ignored.
-static void lttng_ust_itt___itt_event_end_callback(void *btx_handle, void *usr_data, int64_t ts,
-                                                   const char *hostname, int64_t vpid,
-                                                   uint64_t vtid, int __itt_err,
+static void lttng_ust_itt___itt_event_end_callback(void *btx_handle,
+                                                   void *usr_data,
+                                                   int64_t ts,
+                                                   const char *hostname,
+                                                   int64_t vpid,
+                                                   uint64_t vtid,
+                                                   int __itt_err,
                                                    __itt_event event) {
   auto *state = static_cast<data_t *>(usr_data);
   auto &stack = state->event_id_stack[{hostname, vpid, vtid, event}];
@@ -214,10 +247,20 @@ static void lttng_ust_itt___itt_event_end_callback(void *btx_handle, void *usr_d
 // --------------- ITT Metadata API callbacks ----------------------------------
 
 // __itt_metadata_add(domain, id, key, type, count, data)
-static void lttng_ust_itt___itt_metadata_add_callback(
-    void *btx_handle, void *usr_data, int64_t ts, const char *hostname, int64_t vpid, uint64_t vtid,
-    __itt_domain *domain, __itt_id /*id*/, __itt_string_handle *key, __itt_metadata_type type,
-    size_t count, void * /*data*/, size_t /*data_len*/, void *data_val) {
+static void lttng_ust_itt___itt_metadata_add_callback(void *btx_handle,
+                                                      void *usr_data,
+                                                      int64_t ts,
+                                                      const char *hostname,
+                                                      int64_t vpid,
+                                                      uint64_t vtid,
+                                                      __itt_domain *domain,
+                                                      __itt_id /*id*/,
+                                                      __itt_string_handle *key,
+                                                      __itt_metadata_type type,
+                                                      size_t count,
+                                                      void * /*data*/,
+                                                      size_t /*data_len*/,
+                                                      void *data_val) {
   auto *state = static_cast<data_t *>(usr_data);
   if (type == __itt_metadata_unknown || count == 0 || !data_val)
     return;
@@ -231,10 +274,20 @@ static void lttng_ust_itt___itt_metadata_add_callback(
 }
 
 // __itt_metadata_add_with_scope(domain, scope, key, type, count, data)
-static void lttng_ust_itt___itt_metadata_add_with_scope_callback(
-    void *btx_handle, void *usr_data, int64_t ts, const char *hostname, int64_t vpid, uint64_t vtid,
-    __itt_domain *domain, __itt_scope scope, __itt_string_handle *key, __itt_metadata_type type,
-    size_t count, void * /*data*/, size_t /*data_len*/, void *data_val) {
+static void lttng_ust_itt___itt_metadata_add_with_scope_callback(void *btx_handle,
+                                                                 void *usr_data,
+                                                                 int64_t ts,
+                                                                 const char *hostname,
+                                                                 int64_t vpid,
+                                                                 uint64_t vtid,
+                                                                 __itt_domain *domain,
+                                                                 __itt_scope scope,
+                                                                 __itt_string_handle *key,
+                                                                 __itt_metadata_type type,
+                                                                 size_t count,
+                                                                 void * /*data*/,
+                                                                 size_t /*data_len*/,
+                                                                 void *data_val) {
   auto *state = static_cast<data_t *>(usr_data);
   if (type == __itt_metadata_unknown || count == 0 || !data_val)
     return;
@@ -288,7 +341,6 @@ void btx_register_usr_callbacks(void *btx_handle) {
 
   btx_register_callbacks_finalize_processing(btx_handle, &btx_finalize_processing);
 
-  REGISTER_ASSOCIATED_CALLBACK(lttng_ust_itt___itt_string_handle_create);
   REGISTER_ASSOCIATED_CALLBACK(lttng_ust_itt___itt_task_begin);
   REGISTER_ASSOCIATED_CALLBACK(lttng_ust_itt___itt_task_end);
   REGISTER_ASSOCIATED_CALLBACK(lttng_ust_itt___itt_event_create);
