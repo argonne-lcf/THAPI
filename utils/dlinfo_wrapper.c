a55e2845c186b4addf01dc6e11541faeee35ff99
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-11-24 16:36:45 +0000

    Add find_lib_path: find lib path using dlinfo()
    in case whichlib fails.
    
    Co-authored-by: Brice Videau <brice.videau@gmail.com>
    Co-authored-by: Thomas Applencourt <tapplencourt@anl.gov>

---
+++ utils/dlinfo_wrapper.c
@@ -0,0 +1,28 @@
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <link.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/limits.h>
+#include <stdio.h>
+
+const char *find_lib_path(const char* lib_name) {
+  void* handle = dlopen(lib_name, RTLD_LAZY);
+  if (!handle) goto err0;
+
+  struct link_map* lm = NULL;
+  if (dlinfo(handle, RTLD_DI_LINKMAP, &lm) != 0) goto err1;
+
+  char *lib_path = calloc(PATH_MAX + 1, sizeof(char));
+  strncpy(lib_path, lm->l_name, PATH_MAX);
+
+  dlclose(handle);
+
+  return lib_path;
+
+err1:
+  dlclose(handle);
+err0:
+  fprintf(stderr, "libDlinfoWrapper.so: dlopen/dlinfo error: %s\n", dlerror());
+  return NULL;
+}
