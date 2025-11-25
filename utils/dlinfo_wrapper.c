30d751451ab7c6f45aea42f2c87ba3ce32535dbf
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-11-25 22:12:16 +0000

    Refactor `dlinfo_wrapper.c`

--- utils/dlinfo_wrapper.c
+++ utils/dlinfo_wrapper.c
@@ -5,27 +5,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int find_lib_path(const char* lib_name, bool verbose) {
-  void* handle = dlopen(lib_name, RTLD_LAZY);
-  if (!handle) goto err0;
-
-  struct link_map* lm = NULL;
-  if (dlinfo(handle, RTLD_DI_LINKMAP, &lm) != 0) goto err1;
-  fprintf(stdout, "%s", lm->l_name);
-
-  dlclose(handle);
-
-  return 0;
-err1:
-  dlclose(handle);
-err0:
-  if (verbose)
-    fprintf(stderr, "[dlinfo_wrapper] dlopen/dlinfo error: %s\n", dlerror());
-  return 1;
-}
-
 int main(int argc, char *argv[]) {
   // No error checking is done for input arguments. We expect the caller to
   // know what they are doing.
-  return find_lib_path(argv[1], atoi(argv[2]) != 0);
+  void* handle = dlopen(argv[1], RTLD_LAZY);
+  int exit_code = !handle;
+  if (exit_code) goto print_error_and_exit;
+
+  struct link_map* lm = NULL;
+  if ((exit_code = dlinfo(handle, RTLD_DI_LINKMAP, &lm)) == 0)
+    fprintf(stdout, "%s", lm->l_name);
+
+  exit_code |= dlclose(handle);
+print_error_and_exit:
+  if (exit_code && atoi(argv[2]) != 0)
+    fprintf(stderr, "[dlinfo_wrapper] dlopen/dlinfo error: %s\n", dlerror());
+  return exit_code;
 }
