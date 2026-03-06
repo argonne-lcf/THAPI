1cef2fa61a792ba011dbcc78a01ec5dd502118f0
tapplencourt <tapplencourt@anl.gov>
2026-03-06 00:09:15 +0000

    Format shell...

--- backends/ze/utils_spec_update/test_symbol_exported.sh
+++ backends/ze/utils_spec_update/test_symbol_exported.sh
@@ -1,7 +1,5 @@
-
 get_funcs() {
-  readelf -W --dyn-syms "$1" | awk '/GLOBAL/ && /DEFAULT/ && /FUNC/ && $8 ~ /^ze/ {print $8}' | LC_COLLATE=C sort 
+  readelf -W --dyn-syms "$1" | awk '/GLOBAL/ && /DEFAULT/ && /FUNC/ && $8 ~ /^ze/ {print $8}' | LC_COLLATE=C sort
 }
 
-diff -u --label "$1" --label "$2"  <(get_funcs $1) <(get_funcs $2)
-
+diff -u --label "$1" --label "$2" <(get_funcs $1) <(get_funcs $2)
