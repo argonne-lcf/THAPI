5db825fa346ba80b8143a377fc4d59ed56376427
tapplencourt <tapplencourt@anl.gov>
2026-03-31 20:28:03 +0000

    No more cuda header!

--- backends/cuda/extract/cuda_api.h
+++ backends/cuda/extract/cuda_api.h
@@ -1,17 +1,10 @@
 #define __CUDA_API_VERSION_INTERNAL = 1
-#define THAPI_NO_INCLUDE
 
 #include <stdint.h>
 
 #include <cuda.h>
 
-typedef int32_t VdpStatus;
-typedef uint32_t VdpFuncId;
-typedef uint32_t VdpDevice;
-typedef VdpStatus
-VdpGetProcAddress(VdpDevice device, VdpFuncId function_id, void **function_pointer);
-typedef uint32_t VdpVideoSurface;
-typedef uint32_t VdpOutputSurface;
+#include <vdpau/vdpau.h>
 
 #include <cudaVDPAU.h>
 
