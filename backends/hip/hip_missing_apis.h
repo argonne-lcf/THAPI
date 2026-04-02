ef6dd60f2b938618a96e644f4833a004a17cf7f7
tapplencourt <tapplencourt@anl.gov>
2026-04-02 19:23:45 +0000

    Add missing api formating

--- backends/hip/hip_missing_apis.h
+++ backends/hip/hip_missing_apis.h
@@ -3,70 +3,51 @@
 
 hipError_t hipGraphDebugDotPrint(hipGraph_t graph, const char *fName, unsigned int flag);
 
-extern void **__hipRegisterFatBinary(
-    const void *data);
+extern void **__hipRegisterFatBinary(const void *data);
 
-extern void __hipRegisterFunction(
-    void         **modules,
-    const void    *hostFunction,
-    char          *deviceFunction,
-    const char    *deviceName,
-    unsigned int   threadLimit,
-    uint3         *tid,
-    uint3         *bid,
-    dim3          *blockDim,
-    dim3          *gridDim,
-    int           *wSize);
+extern void __hipRegisterFunction(void **modules,
+                                  const void *hostFunction,
+                                  char *deviceFunction,
+                                  const char *deviceName,
+                                  unsigned int threadLimit,
+                                  uint3 *tid,
+                                  uint3 *bid,
+                                  dim3 *blockDim,
+                                  dim3 *gridDim,
+                                  int *wSize);
 
-extern void __hipRegisterManagedVar(
-    void      **modules,
-    void      **pointer,
-    void       *init_value,
-    const char *name,
-    size_t      size,
-    unsigned    align);
+extern void __hipRegisterManagedVar(void **modules,
+                                    void **pointer,
+                                    void *init_value,
+                                    const char *name,
+                                    size_t size,
+                                    unsigned align);
 
-extern void __hipRegisterSurface(
-    void **modules,
-    void  *var,
-    char  *hostVar,
-    char  *deviceVar,
-    int    type,
-    int    ext);
+extern void
+__hipRegisterSurface(void **modules, void *var, char *hostVar, char *deviceVar, int type, int ext);
 
 extern void __hipRegisterTexture(
-    void **modules,
-    void  *var,
-    char  *hostVar,
-    char  *deviceVar,
-    int    type,
-    int    norm,
-    int    ext);
+    void **modules, void *var, char *hostVar, char *deviceVar, int type, int norm, int ext);
 
-extern void __hipRegisterVar(
-    void   **modules,
-    void    *var,
-    char    *hostVar,
-    char    *deviceVar,
-    int      ext,
-    size_t   size,
-    int      constant,
-    int      global);
+extern void __hipRegisterVar(void **modules,
+                             void *var,
+                             char *hostVar,
+                             char *deviceVar,
+                             int ext,
+                             size_t size,
+                             int constant,
+                             int global);
 
-extern void __hipUnregisterFatBinary(
-    void** modules);
+extern void __hipUnregisterFatBinary(void **modules);
 
-extern const char* hipGetCmdName(
-    uint32_t id);
+extern const char *hipGetCmdName(uint32_t id);
 
 typedef enum activity_domain_t {
   ACTIVITY_DOMAIN_HSA_API = 0,
   ACTIVITY_DOMAIN_HSA_OPS = 1,
   ACTIVITY_DOMAIN_HIP_OPS = 2,
-  ACTIVITY_DOMAIN_HCC_OPS =
-      ACTIVITY_DOMAIN_HIP_OPS,
-  ACTIVITY_DOMAIN_HIP_VDI =
-      ACTIVITY_DOMAIN_HIP_OPS,
+  ACTIVITY_DOMAIN_HCC_OPS = ACTIVITY_DOMAIN_HIP_OPS,
+  ACTIVITY_DOMAIN_HIP_VDI = ACTIVITY_DOMAIN_HIP_OPS,
   ACTIVITY_DOMAIN_HIP_API = 3,
   ACTIVITY_DOMAIN_KFD_API = 4,
   ACTIVITY_DOMAIN_EXT_API = 5,
@@ -75,12 +56,9 @@ typedef enum activity_domain_t {
   ACTIVITY_DOMAIN_NUMBER
 } activity_domain_t;
 
-typedef int hipRegisterTracerCallback_callback_t(
-    activity_domain_t domain,
-    uint32_t operation_id,
-    void* data);
+typedef int
+hipRegisterTracerCallback_callback_t(activity_domain_t domain, uint32_t operation_id, void *data);
 
-extern void hipRegisterTracerCallback(
-    hipRegisterTracerCallback_callback_t *function);
+extern void hipRegisterTracerCallback(hipRegisterTracerCallback_callback_t *function);
 
 #endif
